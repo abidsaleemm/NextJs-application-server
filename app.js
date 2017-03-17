428e262ade7d77234be6f058a406c3919dee8a6c
rinstar <codeninja102@gmail.com>
2017-03-16 22:34:57 -0700

    Azure nodejs test fat arrow

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var app = express()
 
 const port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, function () {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 })
 

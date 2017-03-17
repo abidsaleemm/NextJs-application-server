c23cea25e42c788b4d135322c56f01065c738782
rinstar <codeninja102@gmail.com>
2017-03-16 21:32:58 -0700

    More node set up

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const { dispatch } = store;
 
 // Express stuff
 const app = express();
-const port = process.env.PORT || 1337;
+// const port = process.env.PORT || 1337;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);

864c3fa51b11c93374bb5538377398e105b9651b
rinstar <codeninja102@gmail.com>
2017-03-20 14:00:01 -0700

    Added spine module stuff

---
+++ modules/spine/importSegments.js
@@ -0,0 +1,101 @@
+import fs from 'fs';
+
+export default () => {
+  const spinePathName = 'SpinePath';
+  const filePath = `app/modules/spine/models/${spinePathName}.json`;
+  const data = fs.readFileSync(filePath);
+  let retObj = {};
+  if (data) {
+    const metaData = JSON.parse(data);
+    retObj = { ...retObj, ...metaData };
+  }
+
+  let objArray = [];
+  const recurseTree = (bone, name, parent) => {
+    const { tail, children } = bone;
+    const nerveRoot =
+      ((name.search('.L') !== -1) ||
+      (name.search('.R') !== -1));
+
+    // Set default values
+    const radius = nerveRoot ?
+    { x: 0.08 * 50, y: 0.10 * 50 } : { x: 0.11 * 50, y: 0.15 * 50 };
+    const radiusTail = nerveRoot ?
+    { x: 0.08 * 50, y: 0.10 * 50} : { x: 0.11 * 50, y: 0.15 * 50 };
+    const padding = nerveRoot ? 0.12 * 50 : 0.12 * 50;
+    const paddingTail = nerveRoot ? 0.2 * 50 : 0.26 * 50;
+    const offsetRoot = -0.14 * 50;
+    const angle = 0;
+    const vertebraRotation = {
+      x: 0,
+      y: 0,
+      z: 0,
+    };
+    const vertebraOffset = {
+      x: 0,
+      y: 0,
+      z: 0,
+    };
+    const vertebraScale = {
+      x: 1,
+      y: 1,
+      z: 1,
+    };
+
+    const center = {
+      x: tail.x,
+      y: tail.y,
+      z: tail.z,
+    };
+
+    // Make relative
+    if (nerveRoot) {
+      center.x = tail.x - parent.center.x;
+      center.y = 0;
+      center.z = 0;
+      parent.offsetRoot = tail.y - parent.center.y;
+    }
+
+    // Create node
+    let node = {
+      name,
+      radius,
+      center,
+      radiusTail,
+      paddingTail,
+      angle,
+    };
+
+    // Add non root props
+    if (!nerveRoot) {
+      node = {
+        ...node,
+        offsetRoot,
+        padding,
+        vertebraRotation,
+        vertebraOffset,
+        vertebraScale,
+      };
+    }
+
+    if (nerveRoot) {
+      return node;
+    }
+
+    const [childLeft] = Object.entries(children)
+      .filter(([key]) => key.search('.L') >= 0)
+      .map(([key, value]) => recurseTree(value, key, node));
+
+    const [childRight] = Object.entries(children)
+      .filter(([key]) => ke    "create-index": "create-index app/actions && create-index app/reducers && create-index app/modules/spine/actions && create-index app/modules/spine/reducers",
+    "lint": "eslint app *.js",
+    const [child] = Object.entries(children)
+      .filter(([key]) => key.search('.R') === -1 && key.search('.L') === -1 )
+      .map(([key, value]) => recurseTree(value, key, data));
+
+    objArray = [{ ...node, childLeft, childRight }, ...objArray];
+  };
+
+  recurseTree(retObj.C1, 'C1');
+  return objArray;
+};
---
+++ modules/spine/importVertebra.js
@@ -0,0 +1,115 @@
+import * as THREE from 'three';
+import fs from 'fs';
+import sortVertexGroups from './sortVertexGroups';
+
+export default (segments = []) =>
+  segments
+    .map(({ name }) => importVertebra(name))
+
+// issue-65
+const importVertebra = (objectName) => {
+  const filePath = `app/modules/spine/models/${objectName}.json`;
+  const data = fs.readFileSync(filePath);
+  if (data) {
+    const parsed = JSON.parse(data);
+    const {
+      vertices,
+      faces,
+      uvs,
+    } = parsed;
+
+    let { vertexGroups } = parsed;
+
+    // Clean this up with better functional methods
+    let i = 0;
+    const len = uvs.length;
+    const faceUvs = [];
+
+    while (i < len) {
+      const fi = [];
+      fi.push(
+        new THREE.Vector2(
+          uvs[i + 2][0],
+          uvs[i + 2][1]
+        )
+      );
+
+      fi.push(
+        new THREE.Vector2(
+          uvs[i + 1][0],
+          uvs[i + 1][1]
+        )
+      );
+
+      fi.push(
+        new THREE.Vector2(
+          uvs[i][0],
+          uvs[i][1]
+        )
+      );
+
+      faceUvs.push(fi);
+      i += 3;
+    }
+
+    // issue-64
+    const {
+      disc_bottom_inner,
+      disc_bottom_outer,
+      disc_top_inner,
+      disc_top_outer,
+    } = vertexGroups;
+
+    // issue-64
+    vertexGroups = sortVertexGroups({
+      vertices,
+      faces,
+      vertexGroups: {
+        discBottomInner: disc_bottom_inner,
+        discBottomOuter: disc_bottom_outer,
+        discTopInner: disc_top_inner,
+        discTopOuter: disc_top_outer,
+      },
+    });
+
+    // issue-64
+    const {
+      discBottomInner,
+      discBottomOuter,
+      discTopInner,
+      discTopOuter,
+    } = vertexGroups;
+
+    // Use functional methods
+    let discVertexGroups = [];
+    for (let j = 0; j < 14; j += 1) {
+      let v = {};
+      v = discBottomInner !== undefined ?
+        { ...v, discBottomInner: discBottomInner[j] } : { ...v };
+      v = discBottomOuter !== undefined ?
+        { ...v, discBottomOuter: discBottomOuter[j] } : { ...v };
+      v = discTopInner !== undefined ?
+        { ...v, discTopInner: discTopInner[j] } : { ...v };
+      v = discTopOuter !== undefined ?
+        { ...v, discTopOuter: discTopOuter[j] } : { ...v };
+
+      discVertexGroups = [
+        ...discVertexGroups,
+        v,
+      ];
+    }
+
+    return {
+      faces: faces.map(([a, b, c]) => ({ a, b, c })),
+      // faceUvs: [faceUvs],
+      faceUvs,
+      vertices: vertices.map(([x, y, z]) => ({ x, y, z })),
+      visible: true,
+      // vertexGroups,
+      discVertexGroups,
+      // position: { x: 0, y: 0, z: 0 }, // TODO add these back in
+      // rotation: { x: 0, y: 0, z: 0 }, // TODO add matrix instead
+      name: objectName,
+    };
+  }
+};

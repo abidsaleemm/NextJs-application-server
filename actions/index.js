85caa5b30452b9c1de02725dec95f284adf29a68
rinstar <codeninja102@gmail.com>
2017-03-23 23:06:03 -0700

    Updated action index file

--- actions/index.js
+++ actions/index.js
@@ -1,8 +1,10 @@
 // @create-index
 
 export { default as clearDicomStore } from './clearDicomStore.js';
-export { default as dicomScanDirectory } from './dicomScanDirectory.js';
+export { default as dicomScan } from './dicomScan.js';
 export { default as dicomSetDirectory } from './dicomSetDirectory.js';
 export { default as projectAdd } from './projectAdd.js';
+export { default as projectStateUpdate } from './projectStateUpdate.js';
+export { default as projectUpdate } from './projectUpdate.js';
 export { default as projects } from './projects.js';
 

d52247f1a48b43e37570f9693c28272250c8b77c
rinstar <codeninja102@gmail.com>
2017-04-06 00:34:24 -0700

    Removed old project payload implimentation

---
+++ helpers/dicomSelectSeries.js
@@ -0,0 +1,16 @@
+// TODO finish this up
+
+import dicomVolume from './dicomVolume';
+
+export default (dicomImages, studyUID, seriesUID) => {
+  const volumePromise =
+    dicomVolume(dicomImages, studyUID, selectedSeries);
+
+  volumePromise.then(volume => {
+    console.log('Sending volume payload');
+    socket.emit('action', {
+      type: 'VOLUME_SET',
+      volume,
+    });
+  });
+}

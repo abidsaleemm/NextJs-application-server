ae29a8877f61535b75d0e6f103331db78f4c6409
rinstar <codeninja102@gmail.com>
2017-04-06 00:41:08 -0700

    Removed scan directory action

--- actions/dicomSetDirectory.js
+++ actions/dicomSetDirectory.js
@@ -3,9 +3,6 @@ import {
   DICOM_DATA,
 } from '../constants/actionTypes';
 
-import dicomScandirectory from '../helpers/dicomScandirectory';
-
 export default (directory) => (dispatch) => {
   dispatch({ type: DICOM_DIRECTORY, directory });
-  // dispatch(dicomScandirectory());
 };

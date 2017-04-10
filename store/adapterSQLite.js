1f19ebf58a312dcf01eb314ec730f56267516091
rinstar <codeninja102@gmail.com>
2017-04-09 23:58:56 -0700

    Added auto creation of db and projects table
    Added multus.db to gitignore

--- store/adapterSQLite.js
+++ store/adapterSQLite.js
@@ -1,40 +1,59 @@
 import sqlite3 from 'sqlite3';
+import fs from 'fs';
 
-const databasePath = './localdb/multus.db';
+const databasePath = './multus.db';
+
+const createTable = (db) => {
+  const query = `
+    CREATE TABLE IF NOT EXISTS \`projects\` (
+    	\`id\`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    	\`studyuid\`	TEXT NOT NULL,
+    	\`datetime\`	TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    	\`data\`	BLOB
+    );
+  `;
+
+  db.run(query);
+}
 
 // issue-4
 // issue-6
-export default () => ({
-  getProjects: (callback = () => {}) => {
-    try {
-      const db = new sqlite3.Database(databasePath); // issue-5
-      db.all(`
-        SELECT id, studyuid, datetime, data
-        FROM projects p1
-        WHERE id = (SELECT max(id) from projects WHERE p1.studyuid = projects.studyuid)
-        ORDER BY id DESC`, (err, rows = []) => {
-          db.close();
-          callback(null, rows.map(({ data }) =>
-            JSON.parse(data)));
-        });
-    } catch(e) {
-      callback(e);
-    }
-  },
-  setProject: (studyUID = null, data = {}, callback = () => {}) => {
-    try {
-      const query = `
-        INSERT INTO projects(studyuid, data)
-        VALUES('${studyUID}', '${JSON.stringify(data)}')
-      `;
+export default () => {
+  const db = new sqlite3.Database(databasePath); // issue-5
+  createTable(db);
 
-      const db = new sqlite3.Database(databasePath); // issue-5
-      db.run(query, (err) => {
-        db.close();
-        callback(err);
-      });
-    } catch(e) {
-      callback(e)
+  return ({
+    getProjects: (callback = () => {}) => {
+      try {
+        // createTable()
+        db.all(`
+          SELECT id, studyuid, datetime, data
+          FROM projects p1
+          WHERE id = (SELECT max(id) from projects WHERE p1.studyuid = projects.studyuid)
+          ORDER BY id DESC`, (err, rows = []) => {
+            db.close();
+            callback(null, rows.map(({ data }) =>
+              JSON.parse(data)));
+          });
+      } catch(e) {
+        callback(e);
+      }
+    },
+    setProject: (studyUID = null, data = {}, callback = () => {}) => {
+      try {
+        const query = `
+          INSERT INTO projects(studyuid, data)
+          VALUES('${studyUID}', '${JSON.stringify(data)}')
+        `;
+
+        const db = new sqlite3.Database(databasePath); // issue-5
+        db.run(query, (err) => {
+          db.close();
+          callback(err);
+        });
+      } catch(e) {
+        callback(e)
+      }
     }
-  },
-});
+  });
+};

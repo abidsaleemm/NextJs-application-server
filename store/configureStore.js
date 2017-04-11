9b2da74554a47f12462abd0245a51553c95f4c66
rinstar <codeninja102@gmail.com>
2017-04-11 02:20:34 -0700

    Removed unused imports

--- store/configureStore.js
+++ store/configureStore.js
@@ -7,8 +7,6 @@ import {
 import thunk from 'redux-thunk';
 import persistProjects from './persistProjects';
 import * as reducers from '../reducers';
-import adapterSQLite from './adapterSQLite';
-import adapterAzureSQL from './adapterAzureSQL';
 import adapter from './adapter';
 
 const enhancer = compose(

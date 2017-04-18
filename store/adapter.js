3c2bcc6d4ca90a42fb703cfac0e06ba9c1ba1eec
rinstar <codeninja102@gmail.com>
2017-04-18 16:28:17 -0700

    Test new Azure Table Blob adapter

--- store/adapter.js
+++ store/adapter.js
@@ -1,5 +1,6 @@
 if (process.env.NODE_ENV === 'local') {
-  module.exports = require('./adapterJSON'); // eslint-disable-line global-require
+  module.exports = require('./adapterAzureTableBlob'); // eslint-disable-line global-require
 } else {
-  module.exports = require('./adapterJSON'); // eslint-disable-line global-require
+  module.exports = require('./adapterAzureTableBlob'); // eslint-disable-line global-require
+  // module.exports = require('./adapterJSON'); // eslint-disable-line global-require
 }

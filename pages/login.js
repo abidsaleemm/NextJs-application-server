43ee6fd504e33acf60b6cabae4a6d5b63cb2945c
rinstar <codeninja102@gmail.com>
2017-06-21 13:57:42 -0700

    Created login and logout routes and handling

---
+++ pages/login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default (url) => (
+    <div>
+        <div>Multus Login </div >
+        <div>
+            <form action="/auth/local" method="post">
+                <input name="username" id="username" type="text" placeholder="Your username" />
+                <input name="password" id="password" type="password" placeholder="Your password" />
+                <input type="submit" value="Login"/>
+            </form>
+        </div>
+    </div >
+)

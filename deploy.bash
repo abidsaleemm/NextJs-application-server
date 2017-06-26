cf7050f303efb57b71ae58b081da3528a9bb1336
rinstar <codeninja102@gmail.com>
2017-06-25 23:35:23 -0700

    Deploy cleanup

--- deploy.bash
+++ deploy.bash
@@ -4,6 +4,6 @@ scp docker-compose.yml adminuser@multus.westus.cloudapp.azure.com:~/
 sudo docker build . -t hackexpert/multus
 sudo docker push hackexpert/multus
 
-# Do Pull on remore
+# Remote commands
 ssh adminuser@multus.westus.cloudapp.azure.com "sudo docker pull hackexpert/multus"
 ssh adminuser@multus.westus.cloudapp.azure.com "sudo docker-compose up -d --force-recreate --remove-orphans"

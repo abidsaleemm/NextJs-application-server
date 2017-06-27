17eb8a4bf19fda84539a04f538c7deb3a6c2c245
rinstar <codeninja102@gmail.com>
2017-06-27 14:08:42 -0700

    Remove remote command from deploy script

--- deploy.bash
+++ deploy.bash
@@ -6,4 +6,4 @@ sudo docker push hackexpert/multus
 
 # Remote commands
 ssh adminuser@multus.westus.cloudapp.azure.com "sudo docker pull hackexpert/multus"
-ssh adminuser@multus.westus.cloudapp.azure.com "sudo docker-compose up -d --force-recreate --remove-orphans"
+#ssh adminuser@multus.westus.cloudapp.azure.com "sudo docker-compose up -d --force-recreate --remove-orphans"

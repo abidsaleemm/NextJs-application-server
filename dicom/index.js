010dc78981f8da58eccd9162b09274185fb04188
rinstar <codeninja102@gmail.com>
2017-07-06 10:43:12 -0700

    Added four test run modes

--- dicom/index.js
+++ dicom/index.js
@@ -2,7 +2,4 @@
 import * as adapterAzure from './adapterAzure';
 import * as adapterLocal from './adapterLocal';
 
-export { default as parseRaw } from './parseRaw';
-
-// module.exports = adapterAzure;
-module.exports = adapterLocal;
+module.exports = process.env.LOCAL_DICOM !== undefined ? adapterLocal : adapterAzure;
--- projects/index.js
+++ projects/index.js
@@ -1,9 +1,14 @@
 import * as adapterJSON from './adapterJSON';
+import * as adapterAzure from './adapterAzure';
 import createProject from './createProject';
 
-// TODO Handle project state here
+// process.env.LOCAL_PROJECTS
+// module.exports = {
+//     ...adapterJSON,
+//     createProject,
+// };
 
-module.exports = {
-    ...adapterJSON,
-    createProject,
-};
+// import * as adapterAzure from './adapterAzure';
+// import * as adapterLocal from './adapterLocal';
+
+module.exports = process.env.LOCAL_PROJECTS !== undefined ? adapterLocal : adapterAzure;

75d1ac9bddd57941762ae881e7db05b4850db202
ganesh singh <ganeshpapola@gmail.com>
2017-08-11 15:06:31 +0530

    user data from azure table while login

--- auth/local.js
+++ auth/local.js
@@ -2,6 +2,12 @@ import passport from "passport";
 import { Strategy } from "passport-local";
 import users from './users';
 
+import bcrypt from 'bcrypt';
+import azure from 'azure-storage';
+const saltRounds = 10;
+const tableService = azure.createTableService();
+const entGen = azure.TableUtilities.entityGenerator;
+
 export default server => {
   server.use(passport.initialize());
   server.use(passport.session());
@@ -19,6 +25,45 @@ export default server => {
       return done(null, false);
     })
   );
+
+  /*============================================================================================
+  ======================= login user data from azure storage issue =============================
+  =============================================================================================*/
+
+
+  /**
+   * users data from azure table storage implementation
+   * used package --> azure (to fetch user info from table)
+   * used package --> bcrypt for encryption or decryption of passwords.. 
+   */
+
+  // passport.use(
+  //   new Strategy((username, password, done) => {
+  //     const user = {};
+  //     // retrieving from azure table storage
+  //     tableService.retrieveEntity('users', username, username, function (error, result, response) {
+  //       if (!error) {
+  //         // bcrypt compares the hash with password provided
+  //         bcrypt.compare(password, response.body.password, function (err, res) {
+  //           // if it's true then returns username and name provided from azure table data 
+  //           if (res === true) {
+  //             console.log('user found logged in');
+  //             user.username = username;
+  //             user.name = response.body.name;
+  //             user.client = response.body.client;
+  //             return done(null, user);
+  //           } else {
+  //             return done(null, false);
+  //           }
+  //         });
+  //       }
+  //     });
+
+  //   })
+  // );
+
+/*===================================================================================================*/
+/*===================================================================================================*/
 
   passport.serializeUser((user, done) => {
     done(null, user);

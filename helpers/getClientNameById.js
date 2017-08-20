a2280256dd05dc3abb1963fb775d1989a2782d8e
rinstar <codeninja102@gmail.com>
2017-08-19 20:51:45 -0700

    Removed helpers now handled in adapter

--- helpers/getClientNameById.js
+++
@@ -1,9 +0,0 @@
-import users from '../auth/users';
-
-export default id => {
-    const { 0: client } = users()
-        .filter(v => v.id == id) // TODO Handle typing better?
-        .map(({ name }) => name);
-
-    return client;
-}

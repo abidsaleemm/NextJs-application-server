9c3c9410df5ff834094b45bb9af400520fbdf8eb
rinstar <codeninja102@gmail.com>
2017-08-20 20:06:40 -0700

    Cleanup with invoice template

--- helpers/invoiceHtml.js
+++ helpers/invoiceHtml.js
@@ -1,27 +1,38 @@
 import path from 'path';
 import moment from 'moment';
 
-
-export default async(projectDetail) => {
-    const today = moment().format("MM-DD-YYYY");
+export default async ({
+    patientName = '-',
+    location = '-',
+    studyDate = '-',
+    modality = '-',
+    studyName = '-',
+    price = '-',
+    patientID = '-',
+    addrssline1 = '',
+    addrssline2 = '',
+    addrssline3 = '',
+    taxId = '-',
+    invoiceDate = moment().format("MM-DD-YYYY"),
+}) => {
     const htmlFilePath = path.join(__dirname, '../invoiceTemplate/invoiceTemplate.html');
-    var htmlTemplate = fs.readFileSync(htmlFilePath, {
+    let htmlTemplate = fs.readFileSync(htmlFilePath, {
         encoding: "utf-8"
     });
-    htmlTemplate = htmlTemplate.replace(/{invoiceName}/g, projectDetail.patientName ? projectDetail.patientName : '-')
-        .replace(/{invoiceLocation}/g, projectDetail.location ? projectDetail.location : '-')
-        .replace(/{todayDate}/g, today)
-        .replace(/{invoiceStudyDate}/g, projectDetail.studyDate ? projectDetail.studyDate : '-')
-        .replace(/{invoiceModality}/g, projectDetail.modality ? projectDetail.modality : '-')
-        .replace(/{invoiceStudyName}/g, projectDetail.studyName ? projectDetail.studyName : '-')
-        .replace(/{invoicePrice}/g, projectDetail.price ? projectDetail.price : '-')
-        .replace(/{invoicePrice}/g, projectDetail.price ? projectDetail.price : '-')
-        .replace(/{clientId}/g, projectDetail.patientID ? projectDetail.patientID : '-')
-        .replace(/{addrssline1}/g, projectDetail.addrssline1)
-        .replace(/{addrssline2}/g, projectDetail.addrssline2)
-        .replace(/{addrssline3}/g, projectDetail.addrssline3)
-        .replace(/{taxId}/g, projectDetail.taxId);
 
+    htmlTemplate = htmlTemplate
+        .replace(/{invoiceName}/g, patientName)
+        .replace(/{invoiceLocation}/g, location)
+        .replace(/{todayDate}/g, invoiceDate)
+        .replace(/{invoiceStudyDate}/g, studyDate)
+        .replace(/{invoiceModality}/g, modality)
+        .replace(/{invoiceStudyName}/g, studyName)
+        .replace(/{invoicePrice}/g, price)
+        .replace(/{clientId}/g, patientID)
+        .replace(/{addrssline1}/g, addrssline1)
+        .replace(/{addrssline2}/g, addrssline2)
+        .replace(/{addrssline3}/g, addrssline3)
+        .replace(/{taxId}/g, taxId);
 
     return htmlTemplate;
 }
--- routes/invoice.js
+++ routes/invoice.js
@@ -1,39 +1,46 @@
 import authMiddleware from "../auth/middleware";
 import helperInvoiceHtml from "../helpers/invoiceHtml";
-import path from 'path';
-import moment from 'moment';
 import queryProjectDetail from "../query/projectDetail";
-import pdf from 'html-pdf';
+import pdf from "html-pdf";
 
+export default ({ server, app }) => {
+  server.get("/pdf", authMiddleware(), async (req, res) => {
+    const { user: { name, ...user} = {} } = req;
 
+    const projectDetail = await queryProjectDetail({ studyUID: req.query.id });
+    const invoiceDetails = {
+      ...projectDetail,
+      ...user,
+      price: "$300", //  TODO This will work for now but should decide best way to store
+      taxId: "593784049", 
+      // TODO Clean up address and break up into street, city, zip, state, country
+      addrssline1: name,
+      addrssline2: "1020 E PALMDALE ST STE 150",
+      addrssline3: 'TUCSON, AZ 857143309',
+    };
 
+    // TODO Decide best way to name file?
+    const fileName =
+      projectDetail.patientName != "undefined"
+        ? projectDetail.patientName
+        : "patient";
 
-  
+    const htmlTemplate = await helperInvoiceHtml(invoiceDetails);
+    
+    pdf.create(htmlTemplate).toBuffer((err, stream) => {
+      if (err) {
+        res.send(err);
+        return;
+      }
 
-  
-  export default ({ server, app }) => {
-  
-    server.get("/pdf", authMiddleware(), async (req, res) => {
-      const projectDetail =  await queryProjectDetail( { studyUID : req.query.id});
+      res.setHeader(
+        "Content-Disposition",
+        `attachment; filename=${fileName}.pdf`
+      );
 
-      const fileName = projectDetail.patientName != "undefined" ? projectDetail.patientName : "patient";
-      projectDetail.price ="$300";
-      projectDetail.taxId ="593784049";
-      projectDetail.addrssline1 = 'OPEN MIRI SOLUTION LLC';
-      projectDetail.addrssline2 = '1020 E PALMDALE ST STE 150';
-      projectDetail.addrssline3 = 'TUCSON, AZ 857143309';
-      const htmlTemplate =  await helperInvoiceHtml(projectDetail);
-          pdf.create(htmlTemplate).toBuffer(function(err, stream) {
-            if (err) res.send(err);
-            res.setHeader(
-              "Content-Disposition",
-              "attachment; filename= " + fileName + ".pdf"
-            );
-            res.setHeader("Content-Type", "application/pdf");
-            res.setHeader("Content-Length", stream.length);
-            res.status(200).end(stream, "binary");
-          });
-       
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader("Content-Length", stream.length);
+      res.status(200).end(stream, "binary");
     });
-  };
-  
+  });
+};

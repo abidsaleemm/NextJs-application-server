28d4941266ec8e5fab59f2d9893217b6fc6573c3
rinstar <codeninja102@gmail.com>
2017-08-28 12:37:42 -0700

    Additional route headers

--- routes/invoice.js
+++ routes/invoice.js
@@ -5,16 +5,14 @@ import pdf from "html-pdf";
 
 export default ({ server, app }) => {
   server.get("/invoice", authMiddleware(), async (req, res) => {
-    const { user: { name, ...user} = {} } = req;
+    const { user: { name, ...user } = {} } = req;
 
     const projectDetail = await queryProjectDetail({ studyUID: req.query.id });
-    // const projectDetail = {};
-    // console.log (projectDetail);
     const invoiceDetails = {
       ...projectDetail,
       ...user,
       price: "$300", //  TODO This will work for now but should decide best way to store
-      taxId: "593784049", 
+      taxId: "593784049",
       // TODO Clean up address and break up into street, city, zip, state, country
       ownerName: name,
     };
@@ -26,21 +24,25 @@ export default ({ server, app }) => {
         : "patient";
 
     const htmlTemplate = await helperInvoiceHtml(invoiceDetails);
-    
+
     pdf.create(htmlTemplate).toBuffer((err, stream) => {
       if (err) {
         res.send(err);
         return;
       }
 
+      res.setHeader('Content-Transfer-Encoding', 'binary');  
+      res.setHeader("Content-Encoding", "none");
+      res.setHeader("Content-Type", "application/pdf");
+
+      res.setHeader("Content-Length", stream.length);
       res.setHeader(
         "Content-Disposition",
         `attachment; filename=${fileName}.pdf`
       );
 
-      res.setHeader("Content-Type", "application/pdf");
-      res.setHeader("Content-Length", stream.length);
       res.status(200).end(stream, "binary");
     });
   });
 };
+

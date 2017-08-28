5c5bea606496e3625d4be7d7efa046d10385e55e
gaurav sharma <sharma02gaurav@gmail.com>
2017-08-28 19:31:45 +0530

    cleaning up

--- actions/payloadInvoice.js
+++ actions/payloadInvoice.js
@@ -5,14 +5,16 @@ import axios from 'axios';
 // export default async ({studyUID}) => ({ type: PAYLOAD_INVOICE, pdfData: await axios (`http://localhost:3000/invoice/?id=${studyUID}`,{ responseType: 'arraybuffer' })});
 export default (studyUID) => {
 
-	const { fetchAction, payloadInvoice } = actions;
+	const url = `http://localhost:3000/invoice/?id=${studyUID}`;
+	const { fetchAction, payloadInvoice, invoiceModalAction } = actions;
 
 	return dispatch => {
 		dispatch (fetchAction (true));
-		return axios.get (`http://localhost:3000/invoice/?id=${studyUID}`,{ responseType: 'arraybuffer' })
+		return axios.get (url, { responseType: 'arraybuffer' })
 			.then (response => {
 				dispatch ({ type: PAYLOAD_INVOICE, pdfData: response.data, showModal: true});
 				dispatch ( fetchAction (false));
+				dispatch ( invoiceModalAction (true));
 			}, err => console.error (err));
 	}
 }
--- reducers/portal.js
+++ reducers/portal.js
@@ -7,7 +7,7 @@ export const initialState = {
 export default  ( state = initialState, { type, portal = {}, pdfData, showModal = false} ) => {
 	switch ( type ) {
 		case PAYLOAD_PORTAL: return { ...state, ...portal };
-		case PAYLOAD_INVOICE: return {...state, ...portal, pdfData, showModal }
+		case PAYLOAD_INVOICE: return {...state, ...portal, pdfData }
 		case INVOICE_MODAL: return { ...state, showModal }
 		// case PAYLOAD_INVOICE: return Object.assign ({}, state, {...portal, pdfData});
 		default: return state;

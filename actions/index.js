70ced585ec388afa57096990109954eaee57b5cc
rinstar <codeninja102@gmail.com>
2017-08-28 11:55:11 -0700

    Cleaned up actions

--- actions/index.js
+++ actions/index.js
@@ -2,9 +2,9 @@ export { default as payloadProjects } from "./payloadProjects";
 export { default as payloadProjectDetail } from "./payloadProjectDetail";
 export { default as payloadPortal } from "./payloadPortal";
 
+// TODO conbine these to actions into a single action?
 export { default as setProjectClient } from "./setProjectClient";
 export { default as setProjectStatus } from "./setProjectStatus";
-export { default as fetchAction } from "./fetchAction";
 
-export { default as payloadInvoice } from './payloadInvoice';
-export { default as invoiceModalAction } from './modalAction';
+export { default as fetchAction } from "./fetchAction";
+export { default as setInvoice } from './setInvoice';

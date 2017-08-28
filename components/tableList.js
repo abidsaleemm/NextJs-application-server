456b3b411d913033b6034c0eb63d0322a8fcca01
rinstar <codeninja102@gmail.com>
2017-08-28 16:05:10 -0700

    Added fields

--- components/tableList.js
+++ components/tableList.js
@@ -1,27 +1,47 @@
 import React from "react";
-import { Table } from 'reactstrap';
+import { Table, Input } from 'reactstrap';
 import uuid from 'uuid';
 
-export default ({ data = [], headers = [], onRowClick = () => {} }) => (
-    <Table striped hover>
-        <thead>
-            <tr>
-                {headers.map(({ title, id }) =>
-                    <th key={`${title}-${id}`}>{title}</th>
-                )}
-            </tr>
-        </thead>
-        <tbody>
-            {data.map((dataProps) => (
-                <tr key={uuid()} onClick={() => onRowClick(dataProps)}>
-                    {headers
-                        .map(({ id, type, title, action }) => 
-                            ({ data: dataProps[id], type, title, action }))
-                        .map(({ data, type, title, action }) => 
-                            (<td key={uuid()}>{data}</td>))
-                    }
+export default ({ data = [], headers = [], onRowClick = () => { } }) => (
+    <div>
+        <style jsx>
+        {`
+            .fieldFilter {
+                padding: 0.4em;
+            }
+
+            .fieldColor {
+                background: #ddd;
+            }
+        `}
+        </style>
+        <Table striped hover>
+            <thead>
+                <tr>
+                    {headers.map(({ title, id }) =>
+                        <th key={`${title}-${id}`}>{title}</th>
+                    )}
                 </tr>
-            ))}
-        </tbody>
-    </Table>
+                <tr className="fieldColor">
+                    {headers.map(({ id, filter }) => filter !== undefined ?
+                        (<td className="fieldFilter" key={`${id}-filter`}>
+                            <Input type='text' name={`filter-${id}`} placeholder={filter} />
+                        </td>) : <td key={`${id}-filter`} />
+                    )}
+                </tr>
+            </thead>
+            <tbody>
+                {data.map((dataProps) => (
+                    <tr key={uuid()} onClick={() => onRowClick(dataProps)}>
+                        {headers
+                            .map(({ id, type, title, action }) =>
+                                ({ data: dataProps[id], type, title, action }))
+                            .map(({ data, type, title, action }) =>
+                                (<td key={uuid()}>{data}</td>))
+                        }
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
+    </div>
 )

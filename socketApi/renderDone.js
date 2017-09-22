0373bd67ab395af9e36fdbcf49e0f53645454887
rinstar <codeninja102@gmail.com>
2017-09-22 16:29:52 -0700

    Modified renderDone action

--- socketApi/renderDone.js
+++ socketApi/renderDone.js
@@ -1,19 +1,23 @@
-import generateVideo from '../video/generateVideo';
-import cleanupImages from "../video/cleanupImages";
-import cleanupVideo from "../video/cleanupVideo";
+import {
+  generateVideo,
+  cleanupImages,
+  // cleanupVideo,
+  saveVideo,
+} from '../video';
 
 export default async ({ socket, action }) => {
-  const { session, numberImages = 0 } = action;
+  const { session, numberImages = 0, studyUID = '' } = action;
+
+  console.log('studyUID', studyUID);
 
   if (session) {
-
     const ret = await generateVideo({ session, numberImages });
 
     console.log("Render done. Cleaning up.", session, numberImages);
-
     await cleanupImages({ session });
 
-    console.log('Cleanup done');
+    console.log('Cleanup done.  Saving Video');
+    await saveVideo({ studyUID, session }); 
   }
 
   // TODO Should we send a response?

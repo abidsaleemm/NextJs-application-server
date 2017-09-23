e136195df7d18d299930a9dace24466ebd9bb34c
tharon-c <design@tharoncarlson.com>
2017-09-23 11:04:19 -0700

    Fill out invoice PDF on response
    Use fillable PDF as template to fill and pipe in response to client

--- routes/invoice.js
+++ routes/invoice.js
@@ -1,48 +1,33 @@
 import authMiddleware from "../auth/middleware";
-import helperInvoiceHtml from "../helpers/invoiceHtml";
-import queryProjectDetail from "../query/projectDetail";
-import pdf from "html-pdf";
+import pdfFillForm from "pdf-fill-form";
+import path from "path";
+
+const templateFile = path.resolve(
+  __dirname,
+  "../invoiceTemplate/F245-127-000.pdf"
+);
 
 export default ({ server, app }) => {
   server.get("/invoice", authMiddleware(), async (req, res) => {
-    const { user: { name, ...user } = {} } = req;
-
-    const projectDetail = await queryProjectDetail({ studyUID: req.query.id });
-    const invoiceDetails = {
-      ...projectDetail,
-      ...user,
-      price: "$300", //  TODO This will work for now but should decide best way to store
-      taxId: "593784049",
-      // TODO Clean up address and break up into street, city, zip, state, country
-      ownerName: name,
-    };
-
-    // TODO Decide best way to name file?
-    const fileName =
-      projectDetail.patientName != "undefined"
-        ? projectDetail.patientName
-        : "patient";
-
-    const htmlTemplate = await helperInvoiceHtml(invoiceDetails);
-
-    pdf.create(htmlTemplate).toBuffer((err, stream) => {
-      if (err) {
-        res.send(err);
-        return;
-      }
-
-      res.setHeader('Content-Transfer-Encoding', 'binary');  
-      res.setHeader("Content-Encoding", "none");
-      res.setHeader("Content-Type", "application/pdf");
-
-      res.setHeader("Content-Length", stream.length);
-      res.setHeader(
-        "Content-Disposition",
-        `attachment; filename=${fileName}.pdf`
+    pdfFillForm
+      .write(templateFile, { form_to: "It works!" }, { save: "pdf" })
+      .then(
+        result => {
+          const { user: { name, ...user } = {} } = req;
+          const fileName = name;
+          res.setHeader("Content-Transfer-Encoding", "binary");
+          res.setHeader("Content-Encoding", "none");
+          res.setHeader("Content-Type", "application/pdf");
+          res.setHeader("Content-Length", result.length);
+          res.setHeader(
+            "Content-Disposition",
+            `attachment; filename=${fileName}.pdf`
+          );
+          res.status(200).end(result, "binary");
+        },
+        err => {
+          console.log(err);
+        }
       );
-
-      res.status(200).end(stream, "binary");
-    });
   });
 };
-

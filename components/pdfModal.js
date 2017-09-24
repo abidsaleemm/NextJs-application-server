f4f81f6cc2770c351110304c4067d9070e1c7932
tharon-c <design@tharoncarlson.com>
2017-09-24 12:26:21 -0700

    Reformat spaces over tabs

--- components/pdfModal.js
+++ components/pdfModal.js
@@ -1,82 +1,75 @@
-import PDF from 'react-pdf-js';
+import PDF from "react-pdf-js";
 
-export default ({ 
-	url = null, 
-	show = false, 
-	onClose = () => {} 
-}) => (
-	<div className={show ? 'overlay' : 'isHidden'}>
-		<style jsx>
-			{`
-			.isHidden { display: none; }
-			
-			.buttonClose {
-				width: 16px;
-				height: 16px;
-				cursor: pointer;
-				background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAAowAAAKMB8MeazgAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAB5SURBVDiNrZPRCcAwCEQfnUiySAZuF8kSWeH6Yz8KrQZMQAicJ+epAB0YwAmYJKIADLic0/GPPCbQAnLznCd/4NWUFfkgy1VjH8CryA95ApYltAiTRCZxpuoW+gz9WXE6NPeg+ra1UDIxGlWEObe4SGxY5fIxlc75Bkt9V4JS7KWJAAAAAElFTkSuQmCC59ef34356faa7edebc7ed5432ddb673d');
-			}
-			
-			.overlay {
-				display: flex;
-				position: fixed;
-				top: 0;
-				left: 0;
-				width: 100%;
-				height: 100%;
-				overflow-y: scroll; 
-				background: rgba(0,0,0,0.6);
-				justify-content: space-around;
-				align-items: center;
-			}
-			
-			.body {
-				display: flex;
-				flex-direction: column;
-				min-width: 600px;
-				height: 90vh;
-				background: #fff;
-				padding: 15px; 
-			}
+export default ({ url = null, show = false, onClose = () => {} }) => (
+  <div className={show ? "overlay" : "isHidden"}>
+    <style jsx>
+      {`
+        .isHidden {
+          display: none;
+        }
 
-			.header {
-				display: flex;
-				flex-direction: row;
-				justify-content: space-between;
-			}
+        .buttonClose {
+          width: 16px;
+          height: 16px;
+          cursor: pointer;
+          background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAAowAAAKMB8MeazgAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAB5SURBVDiNrZPRCcAwCEQfnUiySAZuF8kSWeH6Yz8KrQZMQAicJ+epAB0YwAmYJKIADLic0/GPPCbQAnLznCd/4NWUFfkgy1VjH8CryA95ApYltAiTRCZxpuoW+gz9WXE6NPeg+ra1UDIxGlWEObe4SGxY5fIxlc75Bkt9V4JS7KWJAAAAAElFTkSuQmCC59ef34356faa7edebc7ed5432ddb673d");
+        }
 
-			.content {
-				display: flex;
-				justify-content: center;
-				height: 100%;
-				padding: 10px;
-			}
+        .overlay {
+          display: flex;
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          overflow-y: scroll;
+          background: rgba(0, 0, 0, 0.6);
+          justify-content: space-around;
+          align-items: center;
+        }
 
-			/*
-			 *	"global" and !impotant are smelly to me.
-			 *	TODO: look at either PDF or CSS alternative
-			*/
-			.content > :global(canvas) {
-				height: 100% !important;
-				width: auto !important;
-			}
-		`}
-		</style>
-		<div className="body">
-			<div className="header">
-				<div>
-					<p>Preview Invoice <a href={url}>Download as pdf</a></p>
-				</div>
-				<div className="buttonClose" onClick={() => onClose()}></div>
-			</div>
-			<div className="content">
-				{url ? (
-					<PDF
-						file={url}
-						page={1}
-					/>
-				): null}
-			</div>
-		</div>
-	</div>
+        .body {
+          display: flex;
+          flex-direction: column;
+          min-width: 600px;
+          height: 90vh;
+          background: #fff;
+          padding: 15px;
+        }
+
+        .header {
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+        }
+
+        .content {
+          display: flex;
+          justify-content: center;
+          height: 100%;
+          padding: 10px;
+        }
+
+        /*
+         * "global" and !impotant are smelly to me.
+         * TODO: look at either PDF or CSS alternative
+        */
+        .content > :global(canvas) {
+          height: 100% !important;
+          width: auto !important;
+        }
+      `}
+    </style>
+    <div className="body">
+      <div className="header">
+        <div>
+          <p>
+            Preview Invoice <a href={url}>Download as pdf</a>
+          </p>
+        </div>
+        <div className="buttonClose" onClick={() => onClose()} />
+      </div>
+      <div className="content">{url ? <PDF file={url} page={1} scale={1}/> : null}</div>
+    </div>
+  </div>
 );

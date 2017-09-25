29c2a95a84f8bd53ad8357ed1bb7605c8ee9b107
rinstar <codeninja102@gmail.com>
2017-09-25 11:49:59 -0700

    Added some boiler plate values that will be used

--- routes/invoice.js
+++ routes/invoice.js
@@ -1,6 +1,7 @@
-import authMiddleware from "../auth/middleware";
 import pdfFillForm from "pdf-fill-form";
 import path from "path";
+import authMiddleware from "../auth/middleware";
+import queryProjectDetail from "../query/projectDetail";
 
 const templateFile = path.resolve(
   __dirname,
@@ -9,25 +10,57 @@ const templateFile = path.resolve(
 
 export default ({ server, app }) => {
   server.get("/invoice", authMiddleware(), async (req, res) => {
-    pdfFillForm
-      .write(templateFile, { form_to: "It works!" }, { save: "pdf" })
-      .then(
-        result => {
-          const { user: { name, ...user } = {} } = req;
-          const fileName = name;
-          res.setHeader("Content-Transfer-Encoding", "binary");
-          res.setHeader("Content-Encoding", "none");
-          res.setHeader("Content-Type", "application/pdf");
-          res.setHeader("Content-Length", result.length);
-          res.setHeader(
-            "Content-Disposition",
-            `attachment; filename=${fileName}.pdf`
-          );
-          res.status(200).end(result, "binary");
-        },
-        err => {
-          console.log(err);
-        }
-      );
+    const { 
+      user: { 
+        // Client data
+        name,
+        address,
+        state,
+        zip,
+        city,
+        phone,
+        ...user 
+      } = {} 
+    } = req;
+
+    // TODO Handle multible studies
+    // This need to be patient distinct
+    const projectDetail = await queryProjectDetail({ studyUID: req.query.id });
+    const { 
+      patientID,
+      patientName,
+      patientBirthDate,
+      patientSex,
+      patientAge,
+      patientWeight,
+      patientAddress,
+      patientTelephoneNumbers,
+      institutionName,
+      referringPhysicianName,
+    } = projectDetail;
+
+    const pdfDetails = {
+      // ...projectDetail,
+      // ...user,
+      pt_name: patientName,
+      form_to: `${name}\r\n${address}`,
+      price: "$300", //  TODO This will work for now but should decide best way to store
+      // taxId: "593784049",
+      // TODO Clean up address and break up into street, city, zip, state, country
+    };
+
+    const result = await pdfFillForm.write(templateFile, pdfDetails, { save: "pdf" })
+
+    const fileName = `${name} - ${patientName}`;
+
+    res.setHeader("Content-Transfer-Encoding", "binary");
+    res.setHeader("Content-Encoding", "none");
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader("Content-Length", result.length);
+    res.setHeader(
+      "Content-Disposition",
+      `attachment; filename=${fileName}.pdf`
+    );
+    res.status(200).end(result, "binary");
   });
 };

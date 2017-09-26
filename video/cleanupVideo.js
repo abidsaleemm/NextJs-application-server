01226409203a0b308defa4d508e17e3e2793f6c9
rinstar <codeninja102@gmail.com>
2017-09-26 02:34:33 -0700

    Small bug with removing video fixed

--- video/cleanupVideo.js
+++ video/cleanupVideo.js
@@ -2,4 +2,10 @@ import fs from 'fs';
 import os from 'os';
 import rimraf from "rimraf";
 
-export default async ({ session }) => fs.unlinkSync(`${os.tmpdir}/${session}.mp4`);
+export default async ({ session }) => {
+  const filePath = `${os.tmpdir}/${session}.mp4`;
+
+  if (fs.existsSync(filePath)) {
+    fs.unlinkSync(filePath);
+  }
+};

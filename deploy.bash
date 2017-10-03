30e246840fa5ad383ac3ac7090c67e2f2ed7f665
rinstar <codeninja102@gmail.com>
2017-10-03 09:44:38 -0700

    Removed manual deploy scripts will use Circle CI from now on for deploy

--- deploy.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build . -t multus/application-server
-docker push multus/application-server
--- staging-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# this will deploy the application-server with staging environment
-
-docker build . -t multus/application-server:staging
-docker push multus/application-server:staging

ac0d5679786e64a34102483c55189aa1f7b0f337
rinstar <codeninja102@gmail.com>
2017-10-04 15:48:55 -0700

    Removed commented code

--- reducers/index.js
+++ reducers/index.js
@@ -3,16 +3,3 @@ export { default as projectDetail } from './projectDetail';
 export { default as portal } from './portal';
 export { default as fetching } from './fetching';
 export { default as serverURL } from './serverURL';
-
-// import { combineReducers } from 'redux';
-// import projects from './projects';
-// import projectDetail from './projectDetail';
-// import portal from './portal';
-// import fetching from './fetching';
-
-// export default combineReducers ({
-// 	projects,
-// 	projectDetail,
-// 	fetching,
-// 	portal,
-// });

9f44f97ea31cad9e9acbe4b0795d1ed43b3f8f7e
rinstar <codeninja102@gmail.com>
2017-10-09 12:43:53 -0700

    Add action comments

--- actions/videoRender.js
+++ actions/videoRender.js
@@ -1,20 +1,7 @@
 import { VIDEO_RENDER } from '../constants/actionTypes';
 
 // TODO handle with server
-
 export default ({ window }) => (dispatch, getState) => {
-    const { } = getState();
-    const url = `/static/render/?p=${studyUID}`;
-    const windowName = 'renderWindow';
-
-    const width = 1920;
-    const height = 1080;
-
-    const settings = `width=${width},height=${height},resizable=false,toolbar=false,status=false`;
-
-    console.log('window open', url, document)
-    // TODO Handled as server process
-    const window = window.open(url, windowName, settings);
-
+    // TODO Finish
     dispatch({ type: VIDEO_RENDER, window });
 }

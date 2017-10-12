fa1e4bdfd346eeef3ac3f0e15d9de284197ce3f2
tharon-c <design@tharoncarlson.com>
2017-10-12 09:07:44 -0700

    stage missing file

---
+++ reducers/projSettings.js
@@ -0,0 +1,44 @@
+import {
+  PROJECTS_SET_FILTER,
+  PROJECTS_SET_SORT
+} from "../constants/actionTypes";
+
+export const initialState = {
+  filter: {
+    status: "",
+    patientName: "",
+    studyName: "",
+    modality: "",
+    location: "",
+    client: ""
+  },
+  sort: {
+    id: "status", // Set default soft id
+    desc: false
+  }
+};
+
+export default (state = initialState, { type, filter = {}, sort = {} }) => {
+  switch (type) {
+    case PROJECTS_SET_FILTER:
+      return {
+        ...state,
+        filter: {
+          ...state.filter,
+          ...filter
+        }
+      };
+    // issue-34 This is reusable cut this reducer up
+    case PROJECTS_SET_SORT:
+      return {
+        ...state,
+        sort: {
+          ...state.sort,
+          ...sort,
+          desc: state.sort.id === sort.id ? !state.sort.desc : state.sort.desc
+        }
+      };
+    default:
+      return state;
+  }
+};

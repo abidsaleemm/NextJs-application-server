7f6b6581d46926f458c7cae503d7f181f7a7d69b
rinstar <codeninja102@gmail.com>
2017-10-17 00:25:09 -0700

    Added comments for a few additional changes

--- selectors/getProjectList.js
+++ selectors/getProjectList.js
@@ -1,18 +1,22 @@
 import R from "ramda";
 
-export default ( data , { filter, sort } ) => {
-  // Filter list by given key and query
-  const filterByKey = (key, query) =>
-    R.filter(item =>
-      R.propSatisfies(
-        x =>
-          !query ? true : x ? R.toLower(x).includes(R.toLower(query)) : false,
-        key,
-        item
-      )
-    );
+// Filter list by given key and query
+// TODO Add this as custom filter under the settings
+// Use this as default text filter
+const filterByKey = (key, query) =>
+  R.filter(item =>
+    R.propSatisfies(
+      x =>
+        !query ? true : x ? R.toLower(x).includes(R.toLower(query)) : false,
+      key,
+      item
+    )
+  );
 
-  // Use filterByKey for each key in settings
+// TODO add custom sort function under settings
+const sortByKey = () => {};
+
+export default (data, { filter, sort }) => {
   const filteredList = list =>
     R.reduce(
       (acc, [key, query]) => filterByKey(key, query)(acc),
@@ -24,7 +28,7 @@ export default ( data , { filter, sort } ) => {
   const sortedList = R.sortBy(R.prop(sort.id));
 
   // Set list order
-  const orderedList = list => sort.desc ? R.reverse(list) : list;
+  const orderedList = list => (sort.desc ? R.reverse(list) : list);
 
-  return R.compose(orderedList, filteredList, sortedList)(data.projects)
+  return R.compose(orderedList, filteredList, sortedList)(data.projects);
 };

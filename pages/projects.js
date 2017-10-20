5a4294ef8f0ea5b2bf37267708b5fef41fbd567e
rinstar <codeninja102@gmail.com>
2017-10-20 11:06:52 -0700

    More cleanups

--- pages/projects.js
+++ pages/projects.js
@@ -33,17 +33,15 @@ class ProjectsListing extends Component {
 		store.dispatch(fetchAction(true));
 		store.dispatch(payloadProjects(isServer ? projects : await fetchApi('projects')));
 		store.dispatch(fetchAction(false));
-
-		return { isServer };
 	}
 
 	render() {
 		const {
 			props: {
         tableData = [],
-				tableHeader = {},
-				tableSettings = {},
-				setProjectsSettings = () => { },
+			tableHeader = {},
+			tableSettings = {},
+			setProjectsSettings = () => { },
 			} = {},
 		} = this;
 
@@ -51,38 +49,40 @@ class ProjectsListing extends Component {
 		const tableDataEnhanced = tableData.map(({ ...project, studyUID, status = '' }) =>
 			({
 				...project,
-				action: status === '' ?
-					// TODO Create as Button dropdown
-					<UncontrolledDropdown>
-						<DropdownToggle caret>
-							Create
+				action: <div style={{ height: "40px" }}>
+					{status === '' ?
+						// TODO Create as Button dropdown
+						<UncontrolledDropdown>
+							<DropdownToggle caret>
+								Create
 					</DropdownToggle>
-						<DropdownMenu>
-							<DropdownItem onClick={() =>
-								Router.push({
-									pathname: '/projectDetail',
-									query: { studyUID }
-								})}
-							>
-								Spine Lumbar
+							<DropdownMenu>
+								<DropdownItem onClick={() =>
+									Router.push({
+										pathname: '/projectDetail',
+										query: { studyUID }
+									})}
+								>
+									Spine Lumbar
 						</DropdownItem>
-							<DropdownItem onClick={() =>
-								Router.push({
-									pathname: '/projectDetail',
-									query: { studyUID }
-								})}
-							>
-								Spine Cervical
+								<DropdownItem onClick={() =>
+									Router.push({
+										pathname: '/projectDetail',
+										query: { studyUID }
+									})}
+								>
+									Spine Cervical
 					</DropdownItem>
 
-					</DropdownMenu>
-					</UncontrolledDropdown> :
-					<Button onClick={() =>
-						Router.push({
-							pathname: '/projectDetail',
-							query: { studyUID }
-						})}
-					>Edit</Button>,
+							</DropdownMenu>
+						</UncontrolledDropdown> :
+						<Button onClick={() =>
+							Router.push({
+								pathname: '/projectDetail',
+								query: { studyUID }
+							})}
+						>Edit</Button>}
+				</div>,
 			}));
 
 		return (
@@ -98,13 +98,13 @@ class ProjectsListing extends Component {
 					}
 				`}
 				</style>
-         <TableList
-          data={tableDataEnhanced}
+				<TableList
+					data={tableDataEnhanced}
 					header={tableHeader}
 					onSort={k => setProjectsSettings({ sortKey: k })}
 					onFilter={([k, v]) => setProjectsSettings({ filter: { [k]: v } })}
 					{...tableSettings}
-        />
+				/>
 			</div>
 		)
 	}
@@ -114,22 +114,23 @@ const mapStateToProps = ({ projectsSettings, projects: { projects } }) => ({
 	tableHeader: {
 		action: { title: "", sort: false },
 		status: { title: "Status", sort: true },
-    patientName: { title: "Patient Name", sort: true },
-    studyName: { title: "Study Name", sort: true },
-    studyDate: { title: "Study Date", sort: true },
-    modality: { title: "Modality", sort: true },
-    location: { title: "Location", sort: true },
-    client: { title: "Client", sort: true },
+		dateReceived: { title: "Received", sort: true },
+		patientName: { title: "Patient Name", sort: true },
+		studyName: { title: "Study Name", sort: true },
+		studyDate: { title: "Study Date", sort: true },
+		modality: { title: "Modality", sort: true },
+		location: { title: "Location", sort: true },
+		client: { title: "Client", sort: true },
 	},
 	tableSettings: projectsSettings,
-  tableData: selectProjectList({ 
+	tableData: selectProjectList({
 		projects,
-		settings: projectsSettings, 
+		settings: projectsSettings,
 	})
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
 
 export default withRedux(initStore, mapStateToProps, mapDispatchToProps)(
-  Wrapper(ProjectsListing)
+	Wrapper(ProjectsListing)
 );

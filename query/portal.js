f442e5403d2a02ef992a1cf658f1773770e3bbe0
rinstar <codeninja102@gmail.com>
2017-10-20 11:07:25 -0700

    Cleanups with queryPortal

--- query/portal.js
+++ query/portal.js
@@ -1,27 +1,39 @@
 import React from 'react';
+import R from 'ramda';
 import { getStudies } from '../dicom';
 import { getProjectList } from '../projects';
 import { videoExists } from '../video';
-
+import { getClients } from '../authUsers';
 import getStatusName from '../helpers/getStatusName';
 
-export default async ({ clientId = 0 }) => {
-    let studies = await getStudies();
+export default async ({ clientId = 0, admin = false }) => {
+    const studies = await getStudies();
+    const clientList = await getClients() || [];
 
     // TODO Do query directly getProjectList instead of filtering with javascript
     let projects = await getProjectList();
     projects = await Promise.all(
         projects
-            .filter(v => v.client == clientId) // TODO fix typing?
+            .filter(v => admin ? true : v.client == clientId) // TODO fix typing?
             .filter(({ studyUID }) => studyUID !== undefined) // Not sure if this will happen?
             .map(project => [project, studies.find(({ studyUID = '' }) => project.studyUID === studyUID)])
             .filter(([project, study]) => study)
-            .map(async ([project, { ...study, studyUID }]) => ({
+            .map(async ([{ status, client, ...project }, { ...study, studyUID }]) => ({
                 ...project,
                 ...study,
-                status: getStatusName(project.status || 0),
+                status: getStatusName(status || 0),
                 videoExists: await videoExists({ studyUID }),
+                client: (({ name = '' }) => name)(clientList.find(({ id }) => id === client) || {})
             })));
 
-    return { projects };
+    const patients = projects.reduce((a, v) => {}, {});
+
+    const test = R.uniqWith((v) => {
+        // console.log('v', v);
+        return v.patientName;
+    })(projects);
+
+    console.log('test', test);
+
+    return { projects, patients };
 }

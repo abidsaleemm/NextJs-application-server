5d0638ccac5e1fe4de79b375bb1359e366617add
rinstar <codeninja102@gmail.com>
2017-10-20 10:44:47 -0700

    Logging middleware tweak

--- store.js
+++ store.js
@@ -4,7 +4,7 @@ import thunk from 'redux-thunk';
 import * as reducers from './reducers';
 
 const enhancer = compose(
-  process.env.NODE_ENV !== "production"
+  process.env.NODE_ENV !== "production" && !process.env.LOCAL
     ? applyMiddleware(thunk, createLogger())
     : applyMiddleware(thunk)
 );

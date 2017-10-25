6d5620d5dfaed648975eca241f19dfc897424b4b
rinstar <codeninja102@gmail.com>
2017-10-25 14:50:29 -0700

    Added upload azure adapter

--- upload/azure.js
+++ upload/azure.js
@@ -1,5 +1,83 @@
-export const get = () => {};
+import azure from "azure-storage";
 
-export const put = () => {};
+const containerName = "uploads";
 
-export const del = () => {};
+// TODO Should be moved to helpers? Reused other places
+const blobService = azure.createBlobService(
+  process.env.STORAGE,
+  process.env.STORAGE_KEY
+);
+
+// TODO Should be moved to helpers? Reused other places
+const createContainerIfNotExists = ({ name }) =>
+  new Promise((resolve, reject) =>
+    blobService.createContainerIfNotExists(
+      name,
+      (error, result, response) => {
+        if (error) {
+          // Container exists and is private
+          console.log("error createContainerIfNotExists", error);
+          reject(error);
+          return;
+        }
+
+        resolve(result);
+      }
+    )
+  );
+
+export const list = async ({ studyUID = "" }) => {
+  await createContainerIfNotExists({ name: containerName });
+  return await new Promise((resolve, reject) => {
+    blobService.listBlobsSegmentedWithPrefix(
+      containerName,
+      studyUID,
+      null,
+      (error, { entries = [] }) => {
+        if (error) {
+          console.log("error list", error);
+          reject(error);
+          return;
+        }
+
+        resolve(
+          entries.map(({ name = "" }) => name.split("/").pop())
+        );
+      }
+    );
+  });
+};
+
+export const get = async ({ studyUID, name }) =>
+  blobService.createReadStream(
+    containerName,
+    `${studyUID}/${name}`,
+    (error, result) => {
+      if (error) {
+        console.log("error get", error);
+      }
+    }
+  );
+
+export const put = async ({ studyUID, name, stream }) => {
+  await createContainerIfNotExists({ name: containerName });
+  return await new Promise((resolve, reject) => {
+    const writeStream = blobService.createWriteStreamToBlockBlob(
+      containerName,
+      `${studyUID}/${name}`,
+      (error, result) => {
+        if (error) {
+          console.log("error put", error);
+          return reject(error);
+        }
+
+        resolve(result);
+      }
+    );
+
+    stream.pipe(writeStream);
+  });
+};
+
+// TODO Get this working with admin account
+export const del = async ({ studyUID, file }) => {};

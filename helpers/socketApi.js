48898f48ed23d4d16d3924ce340cecfab136d71c
rinstar <codeninja102@gmail.com>
2017-10-25 12:02:50 -0700

    Cleanups

--- helpers/socketApi.js
+++ helpers/socketApi.js
@@ -1,30 +1,11 @@
 import io from "socket.io-client";
 
-export default async (action = '', props = {}) => {
+export default async (action = "", props = {}) => {
   const socket = io();
   return await new Promise((resolve, reject) => {
-    socket.emit(
-    action,
-    props,
-    (retProps) => {
-      console.log("socket done");
-      // TODO Call payload action to refresh list
+    socket.emit(action, props, retProps => {
+      // TODO Socketio should always return a response as object, but might change in the future when API gets refactored
       resolve(retProps);
-    }
-  );
-})
-
-    // const res = await fetch(
-    //   '/api',
-    //   {
-    //     credentials: 'same-origin',
-    //     headers: {
-    //       'Accept': 'application/json',
-    //       'Content-Type': 'application/json',
-    //     },
-    //     method: "POST",
-    //     body: JSON.stringify({ action, props })
-    //   })
-      
-    // return await res.json();
+    });
+  });
 };

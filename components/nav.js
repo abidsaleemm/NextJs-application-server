8cc0f439741cd7e74bee9c557b1a446253543c19
rinstar <codeninja102@gmail.com>
2017-10-26 16:52:13 -0700

    Cleanups with STAGING logic

--- components/nav.js
+++ components/nav.js
@@ -1,34 +1,50 @@
-import React, { Component } from 'react';
-import Link from 'next/link';
-import Router from 'next/router';
-import { Nav, NavItem, NavLink } from 'reactstrap';
-import classNames from 'classnames';
+import React, { Component } from "react";
+import Link from "next/link";
+import Router from "next/router";
+import { Nav, NavItem, NavLink } from "reactstrap";
+import classNames from "classnames";
 
-const RenderUser = () =>
-  <div className="buttonLink" onClick={() => Router.push({ pathname: '/projects' })}>
+const RenderUser = () => (
+  <div
+    className="buttonLink"
+    onClick={() => Router.push({ pathname: "/projects" })}
+  >
     Projects
   </div>
+);
 
-const RenderClient = () =>
-  <div className="buttonLink" onClick={() => Router.push({ pathname: '/portal' })}>
+const RenderClient = () => (
+  <div
+    className="buttonLink"
+    onClick={() => Router.push({ pathname: "/portal" })}
+  >
     Portal
   </div>
+);
 
-const RenderAdmin = () =>
+const RenderAdmin = () => (
   <div>
-    <div className="buttonLink" onClick={() => Router.push({ pathname: '/projects' })}>
+    <div
+      className="buttonLink"
+      onClick={() => Router.push({ pathname: "/projects" })}
+    >
       Projects
     </div>
-    <div className="buttonLink" onClick={() => Router.push({ pathname: '/portal' })}>
+    <div
+      className="buttonLink"
+      onClick={() => Router.push({ pathname: "/portal" })}
+    >
       Portal
     </div>
   </div>
+);
 
-export default ({ client, admin = false }) => {
-  // TODO Might be a hack but works well was not able to solve solution
-  const { STAGING: staging } = 'undefined' !== typeof window ? window.env : process.env;
+// TODO Might be a hack but works well was not able to solve solution
+const { STAGING: staging = false } =
+  "undefined" !== typeof window ? window.env : process.env;
 
-  return (<div>
+export default ({ client, admin = false }) => (
+  <div>
     <style jsx global>
       {`
         .buttonLink {
@@ -37,10 +53,10 @@ export default ({ client, admin = false }) => {
           color: white;
           display: inline-block;
         }
-        
+
         .nav {
-          background: #3079C6;
-          box-shadow: 0 2px 5px rgba(0, 0, 0, 0.20);
+          background: ${ staging ? 'red' : '#3079c6' };
+          box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
         }
 
         .navStaging {
@@ -49,8 +65,16 @@ export default ({ client, admin = false }) => {
       `}
     </style>
     <Nav className={classNames({ nav: true, navStaging: staging })}>
-      <Link href="/auth/logout"><a className="buttonLink">Logout</a></Link>
-      {admin ? <RenderAdmin /> : client === true ? <RenderClient /> : <RenderUser />}
+      <Link href="/auth/logout">
+        <a className="buttonLink">Logout</a>
+      </Link>
+      {admin ? (
+        <RenderAdmin />
+      ) : client === true ? (
+        <RenderClient />
+      ) : (
+        <RenderUser />
+      )}
     </Nav>
-  </div>)
-}
+  </div>
+);

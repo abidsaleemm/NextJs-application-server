96f0b2a40c4f48944424e6c524378194fde72a60
rinstar <codeninja102@gmail.com>
2017-10-26 15:49:23 -0700

    Updated all npm packages and resolved problems for now

--- query/portal.js
+++ query/portal.js
@@ -1,5 +1,5 @@
 import React from "react";
-import R from "ramda";
+import * as R from 'ramda';
 import { getStudies } from "../dicom";
 import { getProjectList } from "../projects";
 import { videoExists } from "../video";

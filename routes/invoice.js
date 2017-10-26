e8dcf15965c19152e962211cf0905180372eb9af
rinstar <codeninja102@gmail.com>
2017-10-25 17:14:40 -0700

    Invoice tweaks

--- routes/invoice.js
+++ routes/invoice.js
@@ -2,7 +2,8 @@ import pdfFillForm from "pdf-fill-form";
 import path from "path";
 import authMiddleware from "../auth/middleware";
 import queryProjectDetail from "../query/projectDetail";
-import { getStudiesByPatientID } from '../dicom';
+import { getStudiesByPatientID, getStudy } from "../dicom";
+import { getClientInfo } from "../authUsers";
 
 const templateFile = path.resolve(
   __dirname,
@@ -22,29 +23,28 @@ export default ({ server, app }) => {
         phone,
         ...user
       } = {},
-      query: {
-        id: patientID = 0
-      } = {}
+      query: { id: patientID = 0 } = {}
     } = req;
 
     // Do lookup of patient study just need first record for now
     const studies = await getStudiesByPatientID({ patientID });
-    const { 0: { studyUID: firstStudy = '' } = {} } = studies;
+    const { 0: { studyUID: firstStudy = "" } = {} } = studies;
 
-    // This need to be patient distinct
-    // DO use a different query
-    const projectDetail = await queryProjectDetail({ studyUID: firstStudy });
+    const study = await getStudy({ studyUID: firstStudy });
     const {
       patientName,
       patientBirthDate,
       patientSex,
-      patientAge,
-      patientWeight,
-      patientAddress,
-      patientTelephoneNumbers,
-      institutionName,
-      referringPhysicianName
-    } = projectDetail;
+      clientID = 0
+    } = study;
+
+    const {
+      name: clientName,
+      address: clientAddress,
+      city: clientCity,
+      state: clientState,
+      country: clientCountry
+    } = await getClientInfo({ clientID });
 
     /*
      * TODO: These values are hard coded until we add these fields
@@ -52,24 +52,28 @@ export default ({ server, app }) => {
      * the model fields the same name as the pdf so that we can map 1 to 1
     */
 
+    const taxEIN = "593784049";
+
+    // TODO If multiple studies create multiple charges
+
     const pdfDetails = {
-      insurance_type: "Medicaid",
+      // insurance_type: "Medicaid",
       pt_name: patientName,
-      form_to: `${name}\r\n3333 E. Diego Dr\r\nTucson AZ`,
-      pt_street: "123 W Elm St",
-      pt_city: "Tucson",
-      pt_state: "AZ",
-      pt_zip: "85224",
-      pt_AreaCode: "520",
-      pt_phone: "555-5555",
-      sex: "male",
+      form_to: `${clientName}\r\n${clientAddress}\r\n${clientCity} ${clientState}`,
+      // pt_street: "",
+      // pt_city: "Tucson",
+      // pt_state: "AZ",
+      // pt_zip: "85224",
+      // pt_AreaCode: "520",
+      // pt_phone: "555-5555",
+      sex: patientSex,
       rel_to_ins: "spouse",
       employment: "no",
       pt_accident: "yes",
       other_accident: "no",
       pt_signature: "SIGNATURE ON FILE",
-      ins_name: "Same",
-      ins_street: "Same",
+      ins_name: patientName,
+      // ins_street: "Same",
       ins_dob_mm: "01",
       ins_dob_dd: "01",
       ins_dod_yy: "77",
@@ -81,7 +85,7 @@ export default ({ server, app }) => {
       place1: "76377",
       ch1: "500.00",
       tax_id: "593784049",
-      ssn: "EIN",
+      ssn: taxEIN,
       t_charge: "500.00"
     };
 
@@ -95,7 +99,10 @@ export default ({ server, app }) => {
     res.setHeader("Content-Encoding", "none");
     res.setHeader("Content-Type", "application/pdf");
     res.setHeader("Content-Length", result.length);
-    res.setHeader("Content-Disposition", `inline; filename=${fileName}.pdf`);
+    res.setHeader(
+      "Content-Disposition",
+      `inline; filename=${fileName}.pdf`
+    );
     res.status(200).end(result, "binary");
   });
 };

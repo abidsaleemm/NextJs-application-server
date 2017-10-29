98689e903c16b6506fa8d942d89865b5c922e000
rinstar <codeninja102@gmail.com>
2017-10-28 22:44:44 -0700

    Finished upload refactor

--- socketServer/uploadPut.js
+++ socketServer/uploadPut.js
@@ -1,13 +1,27 @@
-import { put as uploadPut } from '../upload';
+import dataUriToBuffer from "data-uri-to-buffer";
+import { Readable } from 'stream';
+import { put as uploadPut } from "../upload";
+import pagePortal from "./pagePortal";
 
-export default async ({ socket, action: { data, studyUID, name } = {}}) => {
+export default async ({
+  socket,
+  action: { data, studyUID, name } = {},
+  // user: { id: clientID }
+  user,
+}) => {
   const decoded = dataUriToBuffer(data);
-  
-  const stream = new Readable;
+
+  const stream = new Readable();
   stream.push(decoded);
   stream.push(null);
 
+  console.log("uploadPut", studyUID, name, stream);
+
   await uploadPut({ studyUID, name, stream });
-  done("Upload done", name);
+  console.log("Upload done", name);
+
+  pagePortal({ socket, user });
+  //pagePortal
+  // Push payload;
   // TODO just send updated project query
 };

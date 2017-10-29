18fb1c5e5dddcd7fd29fdd3f9904ff9e2798ab8b
rinstar <codeninja102@gmail.com>
2017-10-28 22:45:10 -0700

    Refactored uploadPut action

--- actions/uploadPut.js
+++ actions/uploadPut.js
@@ -1,17 +1,3 @@
-import socketApi from '../helpers/socketApi';
-import fetchApi from '../helpers/fetchApi';
-import fetchAction from './fetchAction';
-import payloadPortal from './payloadPortal';
-
 export default ({ data, name, studyUID = ''}) => dispatch => {
-  dispatch(fetchAction(true));
-  socketApi("uploadPut", { studyUID, name, data })
-    .then(async v => {
-      dispatch(payloadPortal(await fetchApi("portal")));
-      dispatch(fetchAction(false));
-    })
-    .catch(e => {
-      console.log(e);
-      dispatch(fetchAction(false));
-    });
+  dispatch({ type: 'server/uploadPut', data, name, studyUID });
 };

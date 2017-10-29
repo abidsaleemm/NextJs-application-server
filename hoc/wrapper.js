2656a07f25a5fa625b0ebec8fb37a13d9833e5f5
rinstar <codeninja102@gmail.com>
2017-10-29 02:02:51 -0700

    Cleanups

--- hoc/wrapper.js
+++ hoc/wrapper.js
@@ -1,14 +1,10 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { bindActionCreators } from 'redux';
-// import withRedux from "next-redux-wrapper";
-import * as actions from '../actions';
+import { bindActionCreators } from "redux";
+import * as actions from "../actions";
 import Nav from "../components/nav";
 import Styles from "../components/styles";
 import Loader from "../containers/loader"; // TODO Requires a store. Should probably have a check for this.
-
-// TODO This should be an action?
-// import fetchApi from "../helpers/fetchApi";
 
 // TODO Getting ENV vars from server to stay on client requires a hack.  Might be better way in future.
 // Embed in DOM
@@ -51,47 +47,26 @@ const WrapperEnhanced = (WrappedComponent, ...params) =>
         store
       } = props;
 
-      // console.log("store", store);
-      // const admin = false;
-      // const client = false;
-
-      // id: userId = 0
-      // const userId = 0;
-
-      // const state = store.getState();
-      // console.log("state", state);
-
       const { setUser } = actions;
 
       if (isServer !== undefined) {
         const {
           admin = false,
           client = false
-          //   id: userId = 0
         } = isServer
           ? store.dispatch(setUser(user))
           : store.dispatch({ type: "server/getUser" });
 
         return {
           ...WrappedComponent.getInitialProps({
-            ...props,
-            // ...store.getState(),
-            // client,
-            // admin,
-            // userId
-          }),
+            ...props
+          })
           // ...props,
-          // ...store.getState(),
-          // client,
-          // admin,
-          // isServer
         };
       }
 
       // Defaults to log in page
-      return WrappedComponent.getInitialProps({
-        props,
-      });
+      return WrappedComponent.getInitialProps(props);
     };
 
     constructor(props) {
@@ -108,8 +83,7 @@ const WrapperEnhanced = (WrappedComponent, ...params) =>
       return <Enhanced {...props} />;
     }
   };
-dispatch =>
-  bindActionCreators(actions, dispatch)
+dispatch => bindActionCreators(actions, dispatch);
 // import React, { Component } from "react";
 // import withRedux from "next-redux-wrapper";
 // import { connect } from 'react-redux';

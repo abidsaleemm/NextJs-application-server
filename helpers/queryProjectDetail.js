3bfdd149b2cf822460c90ca8a4118df61b0bc76d
rinstar <codeninja102@gmail.com>
2017-10-28 22:46:51 -0700

    Finished projectDetail query

--- helpers/queryProjectDetail.js
+++ helpers/queryProjectDetail.js
@@ -1,1 +1,21 @@
-export default () => {}
+import {
+  getProject,
+  getProjectSnapshot
+} from "../projects";
+import { getClientInfo } from "../authUsers";
+
+export default async ({ studyUID = 0 }) => {
+  const { clientID = 0, ...study } = await getStudy({ studyUID });
+  const project = {
+    ...await getProject({ studyUID }),
+    ...await getProjectSnapshot({ studyUID })
+  };
+
+  // Merge project and study table
+  return {
+    ...project,
+    ...study,
+    studyUID,
+    client: (({ name }) => name)(await getClientInfo({ clientID }))
+  };
+};

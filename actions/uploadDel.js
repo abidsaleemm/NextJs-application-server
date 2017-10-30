d6f6604d9d2f6a5a65319e66184dcacdba7f4ad6
rinstar <codeninja102@gmail.com>
2017-10-29 23:52:29 -0700

    Cleaned up uploadDel action

--- actions/uploadDel.js
+++ actions/uploadDel.js
@@ -1,17 +1,3 @@
-import fetchApi from "../helpers/fetchApi";
-import fetchAction from "./fetchAction";
-import payloadPortal from "./payloadPortal";
-
-export default ({ data, name, studyUID = "" }) => dispatch => new Promise((resolve, reject) => {
-  dispatch(fetchAction(true));
-  fetchApi("uploadDel", { studyUID, name })
-    .then(async v => {
-      dispatch(payloadPortal(await fetchApi("portal")));
-      dispatch(fetchAction(false));
-      resolve();
-    })
-    .catch(e => {
-      console.log(e);
-      dispatch(fetchAction(false));
-    });
-});
+export default ({ name, studyUID = "" }) => dispatch => {
+  dispatch({ type: "server/uploadDel", name, studyUID });
+};

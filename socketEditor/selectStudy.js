becf051cf1ed761bc37124b55ab389f371e60d2b
rinstar <codeninja102@gmail.com>
2017-10-30 22:41:57 -0700

    Added better fetch handling

--- socketEditor/selectStudy.js
+++ socketEditor/selectStudy.js
@@ -15,25 +15,35 @@ export default async ({ socket, action }) => {
   const dicomSeries = await getSeries({ studyUID });
   const { 0: { seriesUID: firstSeriesUID } = [] } = dicomSeries;
 
-  await socket.emit(
-    "action",
-    {
-      type: "PROJECT_PAYLOAD",
-      project: {
-        selectedSeries: firstSeriesUID,
-        ...project,
-        dicomSeries
+  await Promise.all([
+    new Promise(async resolve => {
+      await socket.emit("action", {
+        type: "PROJECT_PAYLOAD",
+        project: {
+          selectedSeries: firstSeriesUID,
+          ...project,
+          dicomSeries
+        }
+      });
+      resolve();
+    }),
+    new Promise(async resolve => {
+      if (dicomSeries.length > 0) {
+        const { selectedSeries = firstSeriesUID } = project;
+        const volume = await getImages({ seriesUID: selectedSeries });
+
+        await socket.emit("action", {
+          type: "VOLUME_SET",
+          volume
+        });
+
+        resolve();
+        return;
       }
-    }
-  );
 
-  if (dicomSeries.length > 0) {
-    const { selectedSeries = firstSeriesUID } = project;
-    const volume = await getImages({ seriesUID: selectedSeries });
+      resolve();
+    })
+  ]);
 
-    socket.emit("action", {
-      type: "VOLUME_SET",
-      volume,
-    });
-  }
+  socket.emit('action', { type: 'SPINNER_TOGGLE', toggle: false });
 };

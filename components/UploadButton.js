b3a37222da97597bf624b147dde647be14f58920
rinstar <codeninja102@gmail.com>
2017-11-07 15:24:53 -0700

    Upload button component refactor and cleanups

--- components/UploadButton.js
+++ components/UploadButton.js
@@ -1,22 +1,50 @@
+import classNames from "classnames";
+
 export default ({
   studyUID = "",
   handleUpload = () => {},
-  hasFiles = false // Just styles so left side has no bevel.
+  hasFiles = false, // Just styles so left side has no bevel.
+  label,
+  style
 }) => {
   const id = `file-upload-${studyUID}`;
 
   return (
-    <div>
-      <label htmlFor={id} style={{ padding: 0, margin: 0 }}>
+    <div style={style ? style : {}}>
+      <style jsx>
+        {`
+          .label {
+            padding: 0;
+            margin: 0;
+          }
+
+          .input {
+            display: none;
+          }
+
+          .hasFiles {
+            border-radius: 0 5px 5px 0;
+          }
+        `}
+      </style>
+      <label
+        htmlFor={id}
+        className="label"
+        style={style ? style : {}}
+      >
         <div
-          className="btn btn-secondary"
-          style={hasFiles ? { borderRadius: "0 5px 5px 0" } : {}}
+          className={classNames({
+            btn: true,
+            "btn-secondary": true,
+            hasFiles
+          })}
+          style={style ? style : {}}
         >
-          Upload File
+          {label ? label : "Upload File"}
         </div>
       </label>
       <input
-        style={{ display: "none" }}
+        className="input"
         id={id}
         type="file"
         onChange={({ target }) => handleUpload({ target, studyUID })}

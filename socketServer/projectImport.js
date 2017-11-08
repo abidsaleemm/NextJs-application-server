c948ab004289c7f1a0d5187dcb43bec58adc20ec
rinstar <codeninja102@gmail.com>
2017-11-08 12:47:32 -0700

    Testing project import and export function on staging

--- socketServer/projectImport.js
+++ socketServer/projectImport.js
@@ -1,43 +1,44 @@
+import { fetchAction } from "../actions";
+import { setProjectSnapshot, getProjectSnapshot } from "../projects";
+import { selectStudy } from "../socketEditor";
+
+// Add props that need to be remove
+const filterRemove = ["studyUID"];
+
 export default async ({
   socket,
+  io,
   action: { studyUID = "", data = "" } = {}
 }) => {
-  // TODO Add better error handling around JSON parsing
-  const parsedData = JSON.parse(data);
+  const parsedData = JSON.parse(data); // TODO Add better error handling around JSON parsing
+  const roomName = `editor/${studyUID}`; // TODO This is reused someplace else.
+
+  // Remap and remove props here.  Maybe use ramda instead?
+  const payload = Object.entries(parsedData).reduce(
+    (a, [k, v]) => ({
+      ...a,
+      ...(filterRemove.some(v => v === k) ? {} : { [k]: v })
+    }),
+    {}
+  );
+
+  const clientSockets = Object.values(
+    io.sockets.sockets
+  ).filter(({ rooms = {} }) =>
+    Object.keys(rooms).some(v => v === roomName)
+  );
 
   await setProjectSnapshot({
     studyUID,
-    payload: parsedData
+    payload
   });
 
-  // TODO Send a broadcast and update the editor payload
-
-
-  // Update SnapShot
-  // if (!studyUID) {
-  //   return;
-  // }
-
-  // console.log("Resetting project to default", studyUID);
-
-  // await socket.emit("action", fetchAction(true)); // TODO move to client
-
-  // const project = createProject({ studyUID }); // TODO Add function to create default from existing
-  // await setProjectSnapshot({
-  //   studyUID,
-  //   payload: project,
-  //   injuryImpingement: {
-  //     visible: false,
-  //     labelVisible: true,
-  //     opacity: 1,
-  //     discs: {}
-  //   }
-  // });
-
-  // await socket.emit(
-  //   "action",
-  //   route({ pathname: "/projectDetail", query: { studyUID } })
-  // );
-
-  // socket.emit("action", fetchAction(false));
+  clientSockets.forEach(async clientSocket => {
+    const { rooms, id } = clientSocket;
+    clientSocket.emit("action", {
+      type: "SPINNER_TOGGLE",
+      toggle: true
+    });
+    await selectStudy({ socket: clientSocket, action: { studyUID } });
+  });
 };

549dd6c97ee4d99df2b846e20b243582c87826db
rinstar <codeninja102@gmail.com>
2017-11-14 15:30:23 -0700

    Changed order of await function calling

--- socketEditor/renderFrame.js
+++ socketEditor/renderFrame.js
@@ -8,10 +8,10 @@ export default async ({ socket, action }) => {
     const output = data.replace(/^data:image\/(png|jpg);base64,/, "");
     const imageBuffer = new Buffer(output, "base64");
 
-    // Send Action back to start render of next frame
-    socket.emit("action", { type: "CAPTURE_FRAME_DONE", index });
-
     console.log('Received frame', session, index);
     await saveImage({ session, index, data: imageBuffer });
+
+    // Send Action back to start render of next frame
+    socket.emit("action", { type: "CAPTURE_FRAME_DONE", index });
   }
 };

415d518291eddc13887cf6957ca9ac2e632d9d6d
rinstar <codeninja102@gmail.com>
2017-11-16 02:47:15 -0700

    Bugs with req deconstructing wrong prop

--- routes/projectDetail.js
+++ routes/projectDetail.js
@@ -1,18 +1,21 @@
 import queryProjectDetail from "../helpers/queryProjectDetail";
-import { getSettings } from '../authUsers';
+import { getSettings } from "../authUsers";
 import authMiddleware from "../auth/middleware";
-import { getDefaultList } from '../defaults';
+import { getDefaultList } from "../defaults";
 
 export default ({ server, app }) =>
-  server.get(
-    "/projectDetail",
-    authMiddleware(),
-    async ({...req, query: { ...query, studyUID = "", user: { id: clientID } } = {} }, res) => {
-      return app.render(req, res, "/projectDetail", {
-        ...query,
-        projectDetail: await queryProjectDetail({ studyUID }),
-        projectDetailSettings: (({ projectDetailSettings }) => projectDetailSettings)(await getSettings(clientID)),
-        defaultList: await getDefaultList(),
-      })
-    }
-  );
+  server.get("/projectDetail", authMiddleware(), async (req, res) => {
+    const {
+      user: { id } = {},
+      query: { studyUID = "", ...query } = {}
+    } = req;
+
+    return app.render(req, res, "/projectDetail", {
+      studyUID,
+      ...query,
+      projectDetail: await queryProjectDetail({ studyUID }),
+      projectDetailSettings: (({ projectDetailSettings }) =>
+        projectDetailSettings)(await getSettings(id)),
+      defaultList: await getDefaultList()
+    });
+  });

74160459e652946ecb0c7329cdac459a255460d6
rinstar <codeninja102@gmail.com>
2017-11-16 02:21:45 -0700

    Removed old fetch api

--- middleware/persistSettings.js
+++ middleware/persistSettings.js
@@ -1,25 +1,18 @@
-import fetchApi from "../helpers/fetchApi";
-import * as R from 'ramda';
+import * as R from "ramda";
 
 export default store => next => action => {
-  const filterWithKeys = pred => R.pipe(
-    R.toPairs,
-    R.filter(R.apply(pred)),
-    R.fromPairs
-  );
+  const filterWithKeys = pred =>
+    R.pipe(R.toPairs, R.filter(R.apply(pred)), R.fromPairs);
 
   const matchSettings = (key, val) => /Settings$/.test(key);
 
-  let result = next(action)
-  if(/SETTINGS$/.test(action.type)) {
-    fetchApi("settings", action ).catch(error => {
-        console.log("oops!",error)
-    });
+  let result = next(action);
+  if (/SETTINGS$/.test(action.type)) {
     store.dispatch({
       type: "server/setSettings",
       action: filterWithKeys(matchSettings)(store.getState())
     });
   }
 
- return result
-}
+  return result;
+};

d5e384c1a64f4d3c0a3200937467b55ee5256bc2
rinstar <codeninja102@gmail.com>
2017-11-16 03:25:22 -0700

    Removed comment code in local user adapter

--- authUsers/local.js
+++ authUsers/local.js
@@ -95,7 +95,6 @@ export const getClientInfo = async ({ clientID = 0 }) =>
       .get("users")
       .find({ id: clientID })
       .value()
-    // users.find(user => user.id == clientID && user.client)
   );
 
 // TODO should only be for admins
@@ -105,26 +104,3 @@ export const getClients = async () => {
     .filter(({ client }) => client !== undefined)
     .map(({ id, name }) => ({ id, name }));
 };
-
-// import low from "lowdb";
-// import FileSync from "lowdb/adapters/FileSync";
-// import settingsStore from "settingsStore";
-// import { users } from "../../authUsers/local";
-// const adapter = new FileSync("./settings/adapterJSON/db.json");
-// const db = low(adapter);
-// const store = settingsStore();
-// const init = store.getState();
-// const getUsers = users.map(user => user.id);
-
-// // Set default state
-// db
-//   .defaults(
-//     getUsers.reduce((acc, user) => ({ ...acc, [user]: init }), {})
-//   )
-//   .write();
-
-// export const setSettings = (userId, settings) => {
-//   db.set(userId, settings).write();
-// };
-
-// export const getSettings = userId => db.get(userId).value();

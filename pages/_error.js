a9dfd3a24f6ed1e2a548ac4c5c1ee68fedc37b26
rinstar <codeninja102@gmail.com>
2017-11-16 22:02:33 -0700

    Added loader in place of error page for now.

---
+++ pages/_error.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import ErrorDefault from "next/error";
+import Loader from "../components/loader";
+
+export default class Error extends Component {
+  static getInitialProps({ res, err }) {
+    const statusCode = res
+      ? res.statusCode
+      : err ? err.statusCode : null;
+    return { statusCode };
+  }
+
+  render() {
+    return <Loader fetching />;
+  }
+}
+
+// <ErrorDefault statusCode={this.props.statusCode} />;

ba7b8cdc562a51b0e53d53a75087c068fe2da03f
rinstar <codeninja102@gmail.com>
2017-12-31 19:39:56 -0700

    Update portal list

--- socketServer/setMetaData.js
+++ socketServer/setMetaData.js
@@ -1,8 +1,19 @@
 import { setMetaData } from "../metaData";
+import queryPortalList from "../helpers/queryPortalList";
+import { payloadPortal } from "../actions";
 
 export default async ({
   socket,
-  action: { studyUID, props } = {}
+  action: { studyUID, props } = {},
+  user: { id: clientID, admin } = {}
 }) => {
   await setMetaData({ studyUID, props });
+  const portalList = await queryPortalList({ clientID, admin });
+
+  await socket.emit(
+    "action",
+    payloadPortal({
+      portalList
+    })
+  );
 };

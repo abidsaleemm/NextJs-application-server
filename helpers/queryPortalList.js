28b11fa09d09a9ad0a17e3a0d55ff3fcabdd033b
rinstar <codeninja102@gmail.com>
2017-12-31 18:33:10 -0700

    Added multusID to remapping props

--- helpers/queryPortalList.js
+++ helpers/queryPortalList.js
@@ -6,43 +6,47 @@ import getStatusName from "./getStatusName";
 import { videoExists } from "../video";
 import { getUserProps } from "../authUsers";
 import { list as uploadList } from "../upload";
+// import { getMetaData } from "../metaData";
 
 const reducePatients = studies =>
-  R.uniqWith((a, b) => a.patientName === b.patientName)(
-    studies
-  ).map(({ client, patientName, patientID, patientBirthDate }) => {
-    return {
-      client,
-      patientName,
-      patientID,
-      patientBirthDate,
-      studies: studies
-        .filter(v => v.patientID === patientID)
-        .map(({ // TODO Refactor to remove unused props?
-          studyUID, studyName, studyDate, status, location, client, videoExists, uploadedFiles }) => ({
-          studyUID,
-          studyName,
-          studyDate,
-          status,
-          statusName: getStatusName(status || 0),
-          location,
-          client,
-          videoExists,
-          uploadedFiles
-        }))
-    };
-  });
+  R.uniqWith((a, b) => a.patientName === b.patientName)(studies).map(
+    ({ client, patientName, patientID, patientBirthDate }) => {
+      return {
+        client,
+        patientName,
+        patientID,
+        patientBirthDate,
+        studies: studies
+          .filter(v => v.patientID === patientID)
+          .map(({ // TODO Refactor to remove unused props?
+            studyUID, studyName, studyDate, status, location, client, videoExists, uploadedFiles, multusID }) => ({
+            studyUID,
+            studyName,
+            studyDate,
+            status,
+            statusName: getStatusName(status || 0),
+            location,
+            client,
+            videoExists,
+            uploadedFiles,
+            multusID
+          }))
+      };
+    }
+  );
 
 export default async ({ clientID, admin } = {}) => {
   const studies = await queryProjectsList({ clientID, admin });
   return reducePatients(
     await Promise.all(
-      studies.map(async ({ studyUID, ...props }) => ({
-        ...props,
-        studyUID,
-        videoExists: await videoExists({ studyUID }),
-        uploadedFiles: await uploadList({ studyUID }) // TODO Move to separate query
-      }))
+      studies.map(async ({ studyUID, ...props }) => {
+        return {
+          ...props,
+          studyUID,
+          videoExists: await videoExists({ studyUID }),
+          uploadedFiles: await uploadList({ studyUID }) // TODO Move to separate query
+        };
+      })
     )
   );
 };

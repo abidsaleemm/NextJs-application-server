5741fc63530c2f5b9531e55190fbda1483329141
rinstar <codeninja102@gmail.com>
2018-01-04 23:58:29 -0700

    Reducer PROJECTDETAIL_SET_SETTINGS merges state and settings

--- reducers/projectDetailSettings.js
+++ reducers/projectDetailSettings.js
@@ -1,10 +1,16 @@
-import { TOGGLE_SIDEBAR_SETTINGS, PROJECTDETAIL_SET_SETTINGS } from "../constants/actionTypes";
+import {
+  TOGGLE_SIDEBAR_SETTINGS,
+  PROJECTDETAIL_SET_SETTINGS
+} from "../constants/actionTypes";
 
 export const initialState = {
   projects: []
 };
 
-export default (state = { sidebarIsOpen: true }, { type, settings }) => {
+export default (
+  state = { sidebarIsOpen: true },
+  { type, settings = {} }
+) => {
   const { sidebarIsOpen, ...rest } = state;
   switch (type) {
     case TOGGLE_SIDEBAR_SETTINGS:
@@ -13,7 +19,7 @@ export default (state = { sidebarIsOpen: true }, { type, settings }) => {
         sidebarIsOpen: !sidebarIsOpen
       };
     case PROJECTDETAIL_SET_SETTINGS:
-      return settings
+      return { ...state, ...settings };
     default:
       return state;
   }

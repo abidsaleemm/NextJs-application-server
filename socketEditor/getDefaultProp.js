51330620206959b85def4c8c9040379a4d00323b
rinstar <codeninja102@gmail.com>
2018-01-04 23:05:47 -0700

    Testing updating vertebra UV on 3d mesh

--- socketEditor/getDefaultProp.js
+++ socketEditor/getDefaultProp.js
@@ -1,5 +1,6 @@
 import { getProject } from "../projects";
 import { getDefault } from "../defaults";
+import createProject from '../projects/createProject';
 
 export default async ({ socket, action }) => {
   const { props = {}, func, studyUID } = action;
@@ -8,7 +9,12 @@ export default async ({ socket, action }) => {
   const project = await getProject({ studyUID });
   if (project) {
     const { defaultName } = project;
-    const defaultState = await getDefault({ name: defaultName });
+    // const defaultState = await getDefault({ name: defaultName });
+    // TODO If no default build props from base models
+    // defaultName
+
+    const defaultState = createProject({ studyUID });
+
     if (defaultState) {
       const actualFunction = new Function(
         `return (${func.toString()})`

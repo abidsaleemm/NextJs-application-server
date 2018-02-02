4970749875d04e3b90515ce6cd5483030fb3c89e
rinstar <codeninja102@gmail.com>
2018-02-02 00:47:02 -0700

    Cleanups

--- projects/adapterAzure/hasProjectSnapshots.js
+++ projects/adapterAzure/hasProjectSnapshots.js
@@ -1,33 +1,6 @@
 import azure from "azure-storage";
 import { blobService, tableName, createContainer } from "./";
 
-// export default async ({ studyUID = "" }) => {
-//   await createContainer();
-
-//   const json = await new Promise((resolve, reject) =>
-//     blobService.getBlobToText(
-//       tableName,
-//       studyUID,
-//       (error, result) => {
-//         if (error) {
-//           reject(error);
-//           return;
-//         }
-
-//         resolve(result);
-//       }
-//     )
-//   );
-
-//   // TODO Catch exceptions for JSON.parse
-//   return JSON.parse(json);
-// };
-
-// import azure from "azure-storage";
-
-// TODO I'm not sure if we even need this now that we are keeping
-// track of files that are already processed and indexed
-
 export default async ({ studyUID = "" }) => {
   await createContainer();
 

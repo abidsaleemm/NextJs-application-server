76c34d9f0881928130029f4a62472390341d7dfb
rinstar <codeninja102@gmail.com>
2018-03-01 14:31:47 -0700

    More testing with transpiler breaking on deployed version

--- loader.js
+++ loader.js
@@ -1,6 +1,10 @@
 // require('babel-register');
 // require('')
-require('babel-core/register');
+// require('babel-core/register');
+require("babel-register")({
+  presets: ["env"]
+});
+
 require("babel-polyfill");
-require('dotenv').config();
-require('./server');
+require("dotenv").config();
+require("./server");

358daf62536d4000f2e16c11c01b5a71f5ebb762
rinstar <codeninja102@gmail.com>
2018-03-19 15:31:47 -0700

    Removed a bunch of old spinal cord default

--- modules/spine/importSegments.js
+++ modules/spine/importSegments.js
@@ -1,6 +1,6 @@
-import fs from 'fs';
+import fs from "fs";
 
-const spinePathName = 'SpinePath';
+const spinePathName = "SpinePath";
 const filePath = `modules/spine/models/${spinePathName}.json`;
 const numRadiusSegments = 16;
 
@@ -16,23 +16,15 @@ export default () => {
   const recurseTree = (bone, name, parent) => {
     const { tail, children } = bone;
     const nerveRoot =
-      ((name.search('.L') !== -1) ||
-      (name.search('.R') !== -1));
+      name.search(".L") !== -1 || name.search(".R") !== -1;
 
-    // Using 8 point array
-    const radius = nerveRoot ? new Array(8).fill(0.08 * 50) : new Array(numRadiusSegments).fill(0.11 * 50);
-    const radiusTail = nerveRoot ? new Array(8).fill(0.08 * 50) : new Array(numRadiusSegments).fill(0.11 * 50);
-
-    // TODO Should padding also be array?
-    const padding = nerveRoot ? 0.12 * 50 : 0.12 * 50;
-    const paddingTail = nerveRoot ? 0.2 * 50 : 0.26 * 50;
+    // TODO We adding this in later?
     const angle = 0;
-    const angleTail = 0;
 
     const center = {
       x: tail.x,
       y: tail.y,
-      z: tail.z,
+      z: tail.z
     };
 
     // Make relative
@@ -40,26 +32,19 @@ export default () => {
       center.x = tail.x - parent.center.x;
       center.y = 0;
       center.z = 0;
-      parent.offsetRoot = tail.y - parent.center.y;
     }
 
     // Create node
     let node = {
       name,
       center,
-      radius,
-      radiusTail,
-      padding,
-      paddingTail,
-      angle,
-      angleTail,
+      angle
     };
 
     // Add non root props
     if (!nerveRoot) {
       node = {
-        ...node,
-        paddingTail,
+        ...node
       };
     }
 
@@ -68,20 +53,22 @@ export default () => {
     }
 
     const [childLeft] = Object.entries(children)
-      .filter(([key]) => key.search('.L') >= 0)
+      .filter(([key]) => key.search(".L") >= 0)
       .map(([key, value]) => recurseTree(value, key, node));
 
     const [childRight] = Object.entries(children)
-      .filter(([key]) => key.search('.R') >= 0)
+      .filter(([key]) => key.search(".R") >= 0)
       .map(([key, value]) => recurseTree(value, key, node));
 
     const [child] = Object.entries(children)
-      .filter(([key]) => key.search('.R') === -1 && key.search('.L') === -1 )
+      .filter(
+        ([key]) => key.search(".R") === -1 && key.search(".L") === -1
+      )
       .map(([key, value]) => recurseTree(value, key, data));
 
     objArray = [{ ...node, childLeft, childRight }, ...objArray];
   };
 
-  recurseTree(retObj.C1, 'C1');
+  recurseTree(retObj.C1, "C1");
   return objArray;
 };

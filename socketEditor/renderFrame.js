c487794fc91bbe9db31b7b98d3e68c4b1bf88ea9
rinstar <codeninja102@gmail.com>
2018-04-03 15:41:45 -0700

    Handles saving duplicate frames

--- socketEditor/renderFrame.js
+++ socketEditor/renderFrame.js
@@ -1,17 +1,24 @@
 import saveImage from "../video/saveImage";
 
 export default async ({ socket, action }) => {
-  const { data, session, index = 0 } = action;
+  const { data, session, index = 0, numFrames = 1 } = action;
 
   if (session) {
     // Decode png image
     const output = data.replace(/^data:image\/(png|jpg);base64,/, "");
     const imageBuffer = new Buffer(output, "base64");
 
-    console.log('Received frame', session, index);
-    await saveImage({ session, index, data: imageBuffer });
+    console.log("Received frame", session, index);
+
+    await Promise.all(
+      new Array(numFrames)
+        .fill()
+        .map((v, i) =>
+          saveImage({ session, index: index + i, data: imageBuffer })
+        )
+    );
 
     // Send Action back to start render of next frame
-    socket.emit("action", { type: "CAPTURE_FRAME_DONE", index });
+    socket.emit("action", { type: "CAPTURE_FRAME_DONE" });
   }
 };

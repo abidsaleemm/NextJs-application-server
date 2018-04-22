8164ae3c0563e7a5ba783b937c5894611bf1b983
rinstar <codeninja102@gmail.com>
2018-04-21 17:28:18 -0700

    Using patient name for filename on export

--- routes/projectExport.js
+++ routes/projectExport.js
@@ -1,11 +1,15 @@
 import authMiddleware from "../auth/middleware";
 import { getProjectSnapshot } from "../projects";
+import { getStudy } from "../dicom";
 
 export default ({ server, app }) => {
   server.get("/export", authMiddleware(), async (req, res) => {
     const { query: { studyUID = "" } = {} } = req;
 
     const project = await getProjectSnapshot({ studyUID });
+
+    // Lookup DICOM tag info
+    const { patientName } = await getStudy({ studyUID });
 
     // TODO Add some sort of try /catch here and handle error
     const serialize = JSON.stringify(project);
@@ -15,7 +19,7 @@ export default ({ server, app }) => {
     res.setHeader("Content-Length", serialize.length);
     res.setHeader(
       "Content-Disposition",
-      `attachment; filename="${studyUID}.json"`
+      `attachment; filename="${patientName}.json"`
     );
     res.status(200).end(serialize, "binary");
   });

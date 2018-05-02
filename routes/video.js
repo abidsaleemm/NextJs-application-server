a137eb817f424c9e6859feaed257de85f8957963
rinstar <codeninja102@gmail.com>
2018-05-02 13:43:04 -0700

    Fixed deconstruction error

--- routes/video.js
+++ routes/video.js
@@ -1,21 +1,17 @@
 import authMiddleware from "../auth/middleware";
-import { videoLoad } from '../video';
+import { videoLoad } from "../video";
 
 export default ({ server, app }) => {
-  server.get(
-    "/video",
-    authMiddleware(),
-    async ({ ...req, user: { client = false }, query }, res) => {
-        const { query: { id, patientName } = {} } = req;
+  server.get("/video", authMiddleware(), async (req, res) => {
+    const { query: { id, patientName } = {} } = req;
 
-        res.setHeader("Content-Type", "video/mp4");
-        res.setHeader(
-            "Content-Disposition",
-            `attachment; filename="${patientName}.mp4"`
-          );
+    res.setHeader("Content-Type", "video/mp4");
+    res.setHeader(
+      "Content-Disposition",
+      `attachment; filename="${patientName}.mp4"`
+    );
 
-        const stream = await videoLoad({ studyUID: id });
-        stream.pipe(res);
-    }
-  );
+    const stream = await videoLoad({ studyUID: id });
+    stream.pipe(res);
+  });
 };

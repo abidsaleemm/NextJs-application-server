d8418870fa409de6c9c31fa2a128c0f27e08ea20
rinstar <codeninja102@gmail.com>
2018-05-04 03:55:37 -0700

    Created videoDelete azure adapter method

--- video/adapter/adapterAzure.js
+++ video/adapter/adapterAzure.js
@@ -2,55 +2,83 @@ import azure from "azure-storage";
 
 // TODO Move this under helpers?  Reusable.
 export const blobService = azure.createBlobService(
-    process.env.STORAGE,
-    process.env.STORAGE_KEY
+  process.env.STORAGE,
+  process.env.STORAGE_KEY
 );
 
-const container = 'videos';
+const container = "videos";
 
 // TODO Move this to default azure helpers?
-const createContainer = () => new Promise((resolve, reject) => {
-    blobService.createContainerIfNotExists(container, (err, result, response) => {
+const createContainer = () =>
+  new Promise((resolve, reject) => {
+    blobService.createContainerIfNotExists(
+      container,
+      (err, result, response) => {
         if (err) {
-            return reject(err);
+          return reject(err);
         }
 
         resolve();
-    });
-});
+      }
+    );
+  });
 
 export const videoSave = async ({ studyUID, readStream }) => {
-    await createContainer(); // Create if container does not exists
-    await new Promise((resolve, reject) => {
-        const writeStream = blobService.createWriteStreamToBlockBlob(container, studyUID, (err, result) => {
-            if (err) {
-                return reject(err);
-            }
+  await createContainer(); // Create if container does not exists
+  await new Promise((resolve, reject) => {
+    const writeStream = blobService.createWriteStreamToBlockBlob(
+      container,
+      studyUID,
+      (err, result) => {
+        if (err) {
+          return reject(err);
+        }
 
-            console.log('Video blob uploaded.', studyUID);
-            resolve(result);
-        })
+        console.log("Video blob uploaded.", studyUID);
+        resolve(result);
+      }
+    );
 
-        readStream.pipe(writeStream);
-    });
-}
+    readStream.pipe(writeStream);
+  });
+};
 
 // Returns readStream
 export const videoLoad = async ({ studyUID }) =>
-    blobService.createReadStream(container, studyUID, (err, result) => {
-        if (err) {
-          console.log(err)
-          return;
-        }
+  blobService.createReadStream(container, studyUID, (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
 
-        console.log(`Video blob loaded ${result.contentLength} bytes.`, studyUID);
-    });
+    console.log(
+      `Video blob loaded ${result.contentLength} bytes.`,
+      studyUID
+    );
+  });
 
-export const videoExists = ({ studyUID }) => new Promise((resolve, reject) =>
-    blobService.doesBlobExist(container, studyUID, (err, { exists }) => {
+export const videoExists = ({ studyUID }) =>
+  new Promise((resolve, reject) =>
+    blobService.doesBlobExist(
+      container,
+      studyUID,
+      (err, { exists }) => {
         if (err) {
-            return reject(err);
+          return reject(err);
         }
 
         resolve(exists);
-    }));
+      }
+    )
+  );
+
+export const videoDelete = ({ studyUID }) =>
+  new Promise((resolve, reject) =>
+    blobService.deleteBlobIfExists(container, studyUID, err => {
+      if (err) {
+        return reject(err);
+      } else {
+        return resolve();
+      }
+    })
+  );

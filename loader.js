1ecc66c76f0a392fa7c731af0cf2a83d7095d28a
rinstar <codeninja102@gmail.com>
2018-05-03 22:35:08 -0700

    Setting up node config

--- loader.js
+++ loader.js
@@ -1,4 +1,17 @@
-require("@babel/register");
+require("@babel/register")({
+  presets: [
+    [
+      "@babel/env",
+      {
+        targets: {
+          node: "10"
+        }
+      }
+    ]
+  ],
+  plugins: [["transform-define", { "process.env.STAGING": "true" }]]
+});
 require("@babel/polyfill");
+
 require("dotenv").config();
 require("./server");

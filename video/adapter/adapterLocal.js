86b1f578424b69d552f60fc8d6b4fcbc04fd7bbc
rinstar <codeninja102@gmail.com>
2018-05-04 02:51:06 -0700

    Setup videoDelete for local adapter

--- video/adapter/adapterLocal.js
+++ video/adapter/adapterLocal.js
@@ -1,20 +1,26 @@
-import fs from 'fs';
+import fs from "fs";
 
-const savePath = 'projectsLocal/video';
+const savePath = "projectsLocal/video";
 
 export const videoSave = async ({ studyUID, readStream }) => {
-    if (fs.existsSync(savePath) === false) {
-        fs.mkdirSync(savePath);
-    }
+  if (fs.existsSync(savePath) === false) {
+    fs.mkdirSync(savePath);
+  }
 
-    const writeStream = fs.createWriteStream(`${savePath}/${studyUID}.mp4`);
-    readStream.pipe(writeStream);
+  const writeStream = fs.createWriteStream(
+    `${savePath}/${studyUID}.mp4`
+  );
+  readStream.pipe(writeStream);
 };
 
 export const videoLoad = ({ studyUID }) => {
-    return fs.createReadStream(`${savePath}/${studyUID}.mp4`);
+  return fs.createReadStream(`${savePath}/${studyUID}.mp4`);
 };
 
 export const videoExists = ({ studyUID }) => {
-    return fs.existsSync(`${savePath}/${studyUID}.mp4`);
+  return fs.existsSync(`${savePath}/${studyUID}.mp4`);
+};
+
+export const videoDelete = ({ studyUID }) => {
+  fs.unlinkSync(`${savePath}/${studyUID}.mp4`);
 };
--- video/index.js
+++ video/index.js
@@ -1,21 +1,27 @@
-import { videoSave, videoLoad, videoExists } from './adapter';
-import cleanup from './cleanup';
-import generateVideo from './generateVideo';
-import saveImage from './saveImage';
-import fs from 'fs';
-import os from 'os';
+import {
+  videoSave,
+  videoLoad,
+  videoExists,
+  videoDelete
+} from "./adapter";
+import cleanup from "./cleanup";
+import generateVideo from "./generateVideo";
+import saveImage from "./saveImage";
+import fs from "fs";
+import os from "os";
 
 module.exports = {
-    generateVideo,
-    cleanup,
-    saveImage,
-    videoLoad,
-    videoExists,
-    videoSave: async ({ session, studyUID }) => {
-        // Create read stream using temp file in tmp dir
-        const videoPath = `${os.tmpdir()}/${session}/video.mp4`;
-        const readStream = fs.createReadStream(videoPath);
+  generateVideo,
+  cleanup,
+  saveImage,
+  videoLoad,
+  videoExists,
+  videoDelete,
+  videoSave: async ({ session, studyUID }) => {
+    // Create read stream using temp file in tmp dir
+    const videoPath = `${os.tmpdir()}/${session}/video.mp4`;
+    const readStream = fs.createReadStream(videoPath);
 
-        return await videoSave({ session, studyUID, readStream });
-    },
+    return await videoSave({ session, studyUID, readStream });
+  }
 };

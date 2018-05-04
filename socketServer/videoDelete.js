501bf255559f094a15b6436394a8ed093341badb
rinstar <codeninja102@gmail.com>
2018-05-04 03:54:19 -0700

    Added video delete handler

--- socketServer/videoDelete.js
+++ socketServer/videoDelete.js
@@ -1,6 +1,6 @@
 import { videoDelete } from "../video";
 import { fetchAction } from "../actions";
-import { pageProjects } from "./pageProjects";
+import pageProjects from "./pageProjects";
 
 export default async ({ socket, action: { studyUID }, user }) => {
   await socket.emit("action", fetchAction(true));

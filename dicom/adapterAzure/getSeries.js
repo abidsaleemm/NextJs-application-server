a020a228c5d380a0c2b38a4deeaac997279536e8
rinstar <codeninja102@gmail.com>
2018-05-07 03:25:22 -0700

    Added direction for series on Azure adapter

--- dicom/adapterAzure/getSeries.js
+++ dicom/adapterAzure/getSeries.js
@@ -1,10 +1,34 @@
+import { Vector3 } from "three";
 import azure from "azure-storage";
-import queryTable from './helpers/queryTable';
-import { tablePrefix } from './';
+import queryTable from "./helpers/queryTable";
+import getImages from "./getImages";
+import { tablePrefix } from "./";
 
-export default async ({ studyUID }) => await queryTable({
+export default async ({ studyUID }) => {
+  const values = await queryTable({
     query: new azure.TableQuery()
-        .select(['seriesName', 'seriesUID'])
-        .where('studyUID eq ?', studyUID),
-    tableName: `${tablePrefix}Series`,
-});
+      .select(["seriesName", "seriesUID"])
+      .where("studyUID eq ?", studyUID),
+    tableName: `${tablePrefix}Series`
+  });
+
+  // Get direction of single image in volume
+  const enhancedValues = await Promise.all(
+    values.map(async v => {
+      const { seriesUID, seriesName } = v;
+      const images = await getImages({ seriesUID });
+      const {
+        [0]: {
+          imageOrientation: [oX, oY] = [
+            { x: 0, y: 0, z: 0 },
+            { x: 0, y: 0, z: 0 }
+          ]
+        } = {}
+      } = images.filter(({ imageOrientation }) => imageOrientation);
+      const direction = new Vector3().crossVectors(oX, oY);
+      return { ...v, direction };
+    })
+  );
+
+  return enhancedValues;
+};

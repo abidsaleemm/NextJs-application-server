9e95b8c5673a9ed7c578497a5f0079e15b90704a
rinstar <codeninja102@gmail.com>
2018-05-07 03:00:29 -0700

    Added direction

--- dicom/adapterLocal/getSeries.js
+++ dicom/adapterLocal/getSeries.js
@@ -1,9 +1,27 @@
-import { series } from './';
+import { Vector3 } from "three";
+import { series } from "./";
+import { images } from "./";
 
-export default async ({ studyUID }) => Object
-    .values(series)
+export default async ({ studyUID }) =>
+  Object.values(series)
     .filter(v => v.studyUID === studyUID)
-    .map(({ seriesUID, seriesName }) => ({
+    .map(({ seriesUID, seriesName }) => {
+      // Get a single image orientation
+      const {
+        [0]: {
+          imageOrientation: [oX, oY] = [
+            { x: 0, y: 0, z: 0 },
+            { x: 0, y: 0, z: 0 }
+          ]
+        } = {}
+      } = Object.values(images).filter(
+        f => f.seriesUID === seriesUID && f.imageOrientation
+      );
+
+      const direction = new Vector3().crossVectors(oX, oY);
+      return {
         seriesUID,
         seriesName,
-    })) || [];
+        direction
+      };
+    }) || [];

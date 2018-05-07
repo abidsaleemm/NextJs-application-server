c5a9e91e00a36135ab281938d8b0b746c88bd91c
rinstar <codeninja102@gmail.com>
2018-05-06 23:57:55 -0700

    Created Status component

---
+++ components/Status.js
@@ -0,0 +1,78 @@
+import React from "react";
+import {
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown
+} from "reactstrap";
+import getStatusName from "../helpers/getStatusName";
+
+export default ({ status, studyUID, setProjectProps = () => {} }) => {
+  return (
+    <UncontrolledDropdown>
+      <DropdownToggle caret>{getStatusName(status)}</DropdownToggle>
+      <DropdownMenu>
+        <DropdownItem
+          onClick={() =>
+            setProjectProps({
+              studyUID,
+              status: 1
+            })
+          }
+        >
+          {getStatusName(1)}
+        </DropdownItem>
+        <DropdownItem
+          onClick={() =>
+            setProjectProps({
+              studyUID,
+              status: 2
+            })
+          }
+        >
+          {getStatusName(2)}
+        </DropdownItem>
+        <DropdownItem
+          onClick={() =>
+            setProjectProps({
+              studyUID,
+              status: 3
+            })
+          }
+        >
+          {getStatusName(3)}
+        </DropdownItem>
+        <DropdownItem
+          onClick={() =>
+            setProjectProps({
+              studyUID,
+              status: 4
+            })
+          }
+        >
+          {getStatusName(4)}
+        </DropdownItem>
+        <DropdownItem
+          onClick={() =>
+            setProjectProps({
+              studyUID,
+              status: 5
+            })
+          }
+        >
+          {getStatusName(5)}
+        </DropdownItem>
+        <DropdownItem
+          onClick={() =>
+            setProjectProps({
+              studyUID,
+              status: 10
+            })
+          }
+        >
+          {getStatusName(10)}
+        </DropdownItem>
+      </DropdownMenu>
+    </UncontrolledDropdown>
+  );
+};

665b0b8f3d7f30f057e7b8481826c8802887121f
rinstar <codeninja102@gmail.com>
2018-05-14 17:35:16 -0700

    Cleanups

--- socketServer/setProjectProps.js
+++ socketServer/setProjectProps.js
@@ -1,11 +1,11 @@
-import { fetchAction } from '../actions';
+import { fetchAction } from "../actions";
 import { setProject } from "../projects";
-import pageProjectDetail from './pageProjectDetail';
+import pageProjectDetail from "./pageProjectDetail";
 
 export default async ({
   socket,
   action: { studyUID, ...props } = {}
 }) => {
   await setProject({ studyUID, props });
-  pageProjectDetail({ socket, action: { studyUID }});
+  pageProjectDetail({ socket, action: { studyUID } });
 };

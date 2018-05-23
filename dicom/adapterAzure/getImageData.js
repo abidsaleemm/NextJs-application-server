a34547bb3fafc0de9c3404c27fe24ebd6c10e057
rinstar <codeninja102@gmail.com>
2018-05-23 15:49:37 -0700

    Fixed bug with image artifact for now

--- dicom/adapterAzure/getImageData.js
+++ dicom/adapterAzure/getImageData.js
@@ -4,6 +4,10 @@ import { tablePrefix } from "./";
 import pullBlob from "./helpers/pullBlob";
 import { tableService } from "./";
 
+function swap16(val) {
+  return ((val & 0xff) << 8) | ((val >> 8) & 0xff);
+}
+
 export default async ({ instanceUID }) => {
   const imageData = await pullBlob({
     container: `${process.env.DICOM_CONTAINER}-cache`,
@@ -13,7 +17,8 @@ export default async ({ instanceUID }) => {
   // TODO Kinda Hacked should use Array methods
   const pixelData = new Array(imageData.length / 2)
     .fill(0)
-    .map((v, i) => imageData[i*2] + (imageData[(i*2)+1] << 8));
+    .map((v, i) => imageData[i * 2] + (imageData[i * 2 + 1] << 8))
+    .map((v, i) => (v & 0x8000 ? 0 : v)); // TODO If last bit set then clear.  This will have to be done on indexer so it's much cleaner
 
   return pixelData;
 };

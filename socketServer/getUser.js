94fc225c3ab6902f1c4076f1f4f852b2d175cfd3
rinstar <codeninja102@gmail.com>
2018-05-30 23:38:37 -0700

    Passing id and users name

--- socketServer/getUser.js
+++ socketServer/getUser.js
@@ -2,13 +2,15 @@ import { setUser } from "../actions";
 
 export default async ({
   socket,
-  user: { admin = false, client = false } = {}
+  user: { admin = false, client = false, id = 0, name = "" } = {}
 }) => {
   socket.emit(
     "action",
     setUser({
       admin,
-      client
+      client,
+      id,
+      name
     })
   );
 };

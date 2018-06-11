ef1de6d00c5ec672b8ba3bf6e019db4003cad7ae
rinstar <codeninja102@gmail.com>
2018-06-11 07:45:02 -0700

    Added SET_PROJECT_PROPS reducer handling

--- reducers/projects.js
+++ reducers/projects.js
@@ -1,14 +1,34 @@
 import {
-	PAYLOAD_PROJECTS,
-} from '../constants/actionTypes';
+  PAYLOAD_PROJECTS,
+  SET_PROJECT_PROPS
+} from "../constants/actionTypes";
 
 export const initialState = {
-	projects: [],
+  projects: []
 };
 
-export default (state = initialState, { type, projects = [] }) => {
-	switch (type) {
-		case PAYLOAD_PROJECTS: return { ...state, projects };
-		default: return state;
-	}
-}
+export default (
+  state = initialState,
+  { type, projects = [], studyUID, ...props }
+) => {
+  switch (type) {
+    case PAYLOAD_PROJECTS:
+      return { ...state, projects };
+    case SET_PROJECT_PROPS: {
+      const { projects } = state;
+      const index = projects.findIndex(v => v.studyUID === studyUID);
+
+      return index >= 0
+        ? {
+            projects: [
+              ...projects.slice(0, index),
+              { ...projects[index], ...props },
+              ...projects.slice(index + 1)
+            ]
+          }
+        : state;
+    }
+    default:
+      return state;
+  }
+};

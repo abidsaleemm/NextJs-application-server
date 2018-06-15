066c0911d38aa9704d61cb8120476afc1f5af87b
rinstar <codeninja102@gmail.com>
2018-06-15 03:16:26 -0700

    Removed unused helper function

--- helpers/asyncForEach.js
+++
@@ -1,9 +0,0 @@
-export default async (array, callback) => {
-  try {
-    for (let i = 0, { length } = array; i < length; i++) {
-      await callback(array[i], i, array);
-    }
-  } catch (error) {
-    throw error;
-  }
-};

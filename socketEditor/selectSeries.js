e2112e997d184c6c647a7085bd61e01c24102cca
rinstar <codeninja102@gmail.com>
2018-06-15 03:16:34 -0700

    Added comment

--- socketEditor/selectSeries.js
+++ socketEditor/selectSeries.js
@@ -60,6 +60,7 @@ export default async ({
     }))
     .filter(({ index }) => index !== sliceLocation);
 
+  // TODO Sort images from sliceLocation outward
   const pool = new PromisePool(() => {
     if (imageListEnhanced.length <= 0) {
       return null;

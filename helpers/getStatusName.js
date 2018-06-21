1baa3a7d252647d3fa7ab379af6d21d7ffd54772
Warren Goble <warren@multusmeical.com>
2018-06-21 14:08:57 -0700

    Changed Start to Pending

--- helpers/getStatusName.js
+++ helpers/getStatusName.js
@@ -2,7 +2,7 @@ export default (status = 0) =>
   ((
     statusNames = {
       0: "None",
-      1: "Start",
+      1: "Pending",
       2: "Segmentation",
       3: "Injuries",
       4: "Review",

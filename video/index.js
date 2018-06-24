34da1aab27781bcca794c18db0a7702a02036963
rinstar <codeninja102@gmail.com>
2018-06-24 02:33:36 -0700

    Old video adaper code refactored
    Still using helper functions for some tasks
    Data functions are now cut up

--- video/index.js
+++ video/index.js
@@ -1,32 +1,10 @@
-//
-
-import {
-  videoSave,
-  videoLoad,
-  videoExists,
-  videoDelete
-} from "./adapter";
-
 // TODO Move to helpers?
 import cleanup from "./cleanup";
 import generateVideo from "./generateVideo";
 import saveImage from "./saveImage";
-import fs from "fs";
-import os from "os";
 
 module.exports = {
   generateVideo,
   cleanup,
-  saveImage,
-  videoLoad,
-  videoExists,
-  videoDelete,
-  // TODO Where is best place to put this?
-  videoSave: async ({ session, studyUID }) => {
-    // Create read stream using temp file in tmp dir
-    const videoPath = `${os.tmpdir()}/${session}/video.mp4`;
-    const readStream = fs.createReadStream(videoPath);
-
-    return await videoSave({ session, studyUID, readStream });
-  }
+  saveImage
 };

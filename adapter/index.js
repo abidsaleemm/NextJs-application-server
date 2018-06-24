6223bc016f8e74df0a971dbac1889e444db5177f
rinstar <codeninja102@gmail.com>
2018-06-24 12:31:11 -0700

    Compose higher order adapter wrappers

--- adapter/index.js
+++ adapter/index.js
@@ -1,10 +1,15 @@
 import azure from "./azure";
 import local from "./local";
 import dicom from "./dicom";
+import video from "./video";
+import { compose } from "ramda";
 
 export default () => {
   const enhancer =
     process.env.LOCAL !== undefined ? local() : azure();
 
-  return dicom(enhancer);
+  return compose(
+    dicom,
+    video
+  )(enhancer);
 };

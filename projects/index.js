160e9455183e5086194547519d497a06f5aa2a39
rinstar <codeninja102@gmail.com>
2018-06-24 02:32:01 -0700

    Removal of old adapter code

--- projects/index.js
+++
@@ -1,6 +0,0 @@
-// TODO Use a fnction to create adapter and import from different location
-
-module.exports =
-  process.env.LOCAL !== undefined
-    ? require("./adapterJSON")
-    : require("./adapterAzure");
--- video/adapter/index.js
+++
@@ -1,4 +0,0 @@
-module.exports =
-  process.env.LOCAL !== undefined
-    ? require("../../adapter/local/video/index")
-    : require("../../adapter/azure/video/index");

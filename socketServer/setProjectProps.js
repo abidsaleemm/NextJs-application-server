1ab88c4ad58be1b0e6e305d3fc431a6049c8173a
rinstar <codeninja102@gmail.com>
2018-06-26 20:16:03 -0700

    Deconstruct removing redux type

--- socketServer/setProjectProps.js
+++ socketServer/setProjectProps.js
@@ -1,6 +1,8 @@
 import { adapter } from "../server";
 
-export default async ({ action: { studyUID, ...props } = {} }) => {
+export default async ({
+  action: { studyUID, type, ...props } = {}
+}) => {
   const { projects: { setProject = () => {} } = {} } = adapter;
 
   setProject({ studyUID, props });

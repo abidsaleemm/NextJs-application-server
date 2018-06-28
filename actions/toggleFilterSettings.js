cb414441e78e808890e412e82a469499ea6eb9a9
rinstar <codeninja102@gmail.com>
2018-06-27 21:28:44 -0700

    Fixed bug with toggleFiltrSettings

--- actions/toggleFilterSettings.js
+++ actions/toggleFilterSettings.js
@@ -2,13 +2,13 @@ import setProjectsSettings from "./setProjectsSettings";
 
 export default (key = "") => (dispatch, getState) => {
   const {
-    projectsSettings: { filter, filter: { [key]: value } = {} } = {}
+    projectsSettings: {
+      filter,
+      filter: { [key]: value = false } = {}
+    } = {}
   } = getState();
 
-  // TODO Check type?
-  if (value !== undefined) {
-    dispatch(
-      setProjectsSettings({ filter: { ...filter, [key]: !value } })
-    );
-  }
+  dispatch(
+    setProjectsSettings({ filter: { ...filter, [key]: !value } })
+  );
 };

0664ce637f9be9e4cfc67bc9c754bfab184ba10c
rinstar <codeninja102@gmail.com>
2018-07-16 23:27:41 -0700

    Fixed video path issue

--- socketEditor/renderDone.js
+++ socketEditor/renderDone.js
@@ -1,10 +1,12 @@
-import { generateVideo, cleanup, videoSave } from "../video";
+import { generateVideo, cleanup } from "../video";
 import { adapter } from "../server";
+import os from "os";
+import fs from "fs";
 
 export default async ({ socket, action }) => {
   const {
     projects: { setProject = () => {} } = {},
-    video: { videoSave = () => {} } = {}
+    file: { put: filePut = () => {} }
   } = adapter;
 
   const { session, numberImages = 0, studyUID = "" } = action;
@@ -20,10 +22,14 @@ export default async ({ socket, action }) => {
     socket.emit("action", { type: "CAPTURE_CLOSE" });
 
     try {
+      // TODO Handle as stream in future.
       const ret = await generateVideo({ session, numberImages });
 
       console.log("Saving Video.");
-      await videoSave({ studyUID, session });
+      const path = `${studyUID}/video.mp4`; // TODO create filename based on
+      const videoPath = `${os.tmpdir()}/${session}/video.mp4`;
+      const readStream = fs.createReadStream(videoPath);
+      await filePut({ path, readStream });
 
       console.log("Video saved cleaning up resources.");
       await cleanup({ session });

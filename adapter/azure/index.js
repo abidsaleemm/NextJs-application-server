65385a6a6fac972a89e7723bcdca4b97079180c0
rinstar <codeninja102@gmail.com>
2018-07-29 22:07:20 -0700

    Added table and blob exiported wrapped functions

--- adapter/azure/index.js
+++ adapter/azure/index.js
@@ -21,6 +21,9 @@ export default ({ storageAccount, storageKey }) => {
     }),
     //
     file: file({ blobAdapter }),
-    users: users({ tableAdapter })
+    users: users({ tableAdapter }),
+    // API specific
+    blob: blobAdapter,
+    table: tableAdapter
   };
 };

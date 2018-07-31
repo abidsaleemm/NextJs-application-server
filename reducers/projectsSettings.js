2d3dd7daa1786746921cbc25823c59a994e5ebcd
rinstar <codeninja102@gmail.com>
2018-07-30 18:37:11 -0700

    Updated projectsSettings reducer

--- reducers/projectsSettings.js
+++ reducers/projectsSettings.js
@@ -1,22 +1,24 @@
 import { PROJECTS_SET_SETTINGS } from "../constants/actionTypes";
 
 const initialState = {
+  //
   filter: {},
   sortKey: "status",
   sortDesc: false,
+  //
   projectsListSortKey: "status",
   projectsListSortDesc: false
 };
 
-export default (
-  state = initialState,
-  { type, settings: { sortKey, filter = {}, ...settings } = {} }
-) => {
+export default (state = initialState, { type, settings = {} }) => {
+  const { filter, sortKey, projectsListSortKey } = settings;
+
   switch (type) {
     // TODO This is used in two places.  Should a utility/helper function?
     case PROJECTS_SET_SETTINGS:
       return {
         ...state,
+        ...settings,
         sortKey: sortKey !== undefined ? sortKey : state.sortKey,
         sortDesc:
           sortKey !== undefined // TODO Handle this in action?
@@ -24,13 +26,20 @@ export default (
               ? !state.sortDesc
               : state.sortDesc
             : state.sortDesc,
-        ...settings,
-        // TODO Handle this in an action instead
-        // Merge filters if any
         filter: {
-          ...state.filter,
-          ...filter
-        }
+          ...filter,
+          ...state.filter
+        },
+        projectsListSortKey:
+          projectsListSortKey !== undefined
+            ? projectsListSortKey
+            : state.projectsListSortKey,
+        projectsListSortDesc:
+          projectsListSortKey !== undefined // TODO Handle this in action?
+            ? state.projectsListSortKey === projectsListSortKey
+              ? !state.projectsListSortDesc
+              : state.projectsListSortDesc
+            : state.projectsListSortDesc
       };
     default:
       return state;

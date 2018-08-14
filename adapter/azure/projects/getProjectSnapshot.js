76de76ed96f91969595397def73d5cbfc1d52fb0
rinstar <codeninja102@gmail.com>
2018-08-14 14:20:12 -0700

    Check existing blob firstt before trying to grab

--- adapter/azure/projects/getProjectSnapshot.js
+++ adapter/azure/projects/getProjectSnapshot.js
@@ -1,13 +1,14 @@
-export default async ({
-  studyUID = "",
-  containerName,
-  blobAdapter: { getBlobToText }
-}) => {
+export default async ({ studyUID = "", containerName, blobAdapter: { getBlobToText, doesBlobExist } }) => {
+  const exists = await doesBlobExist({ blobName: studyUID, containerName });
+
+  if (!exists) {
+    return;
+  }
   const json = await getBlobToText({
     containerName,
     blobName: studyUID
   });
 
-  // TODO Catch exceptions for JSON.parse
+  // TODO Catch exceptions for JSON.parse? WG
   return JSON.parse(json);
 };

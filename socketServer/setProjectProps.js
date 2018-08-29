4e6dd69abbd91287c7b307b56c82325958cda9b9
rinstar <codeninja102@gmail.com>
2018-08-29 16:53:14 -0700

    Fixed bug with setting status to Done

--- socketServer/setProjectProps.js
+++ socketServer/setProjectProps.js
@@ -1,12 +1,20 @@
 import { adapter } from "../server";
 
-export default async ({
-  action: { studyUID, type, ...props } = {}
-}) => {
-  
-  const { projects: { setProject = () => {} } = {}, renders: { setRender = () => {} } = {} } = adapter;
-  if(props.status === "Done")
+export default async ({ action: { studyUID, type, ...props } = {} }) => {
+  const {
+    projects: { setProject = () => {} } = {},
+    renders: { setRender = () => {} } = {}
+  } = adapter;
+
+  const { status } = props;
+
+  if (status === "Done") {
+    // TODO Will need to add extra template for images
+    // A single template should also be passed on
+    // Also if there is alrady an existing render thats the
+    // same don't add to queue
     await setRender({ studyUID });
-  else  
-    setProject({ studyUID, props });
+  }
+
+  setProject({ studyUID, props });
 };

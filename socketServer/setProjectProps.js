b884cabe11fa6565709f8b74eb7c3924216467eb
DevrajG <rohannepal200@gmail.com>
2018-08-29 20:54:02 -0400

    Added 6 entities to setRender

--- socketServer/setProjectProps.js
+++ socketServer/setProjectProps.js
@@ -6,7 +6,14 @@ export default async ({
   
   const { projects: { setProject = () => {} } = {}, renders: { setRender = () => {} } = {} } = adapter;
   if(props.status === "Done")
-    await setRender({ studyUID });
+  {
+    await setRender({ p: studyUID });
+    await setRender({ p: studyUID, debug: true });
+    await setRender({ p: studyUID, anonymous: true });
+    await setRender({ p: studyUID, template: "spineImages" });
+    await setRender({ p: studyUID, template: "spineImages", anonymouse: true });
+    await setRender({ p: studyUID, template: "spineComparison" });
+  }
   else  
     setProject({ studyUID, props });
 };

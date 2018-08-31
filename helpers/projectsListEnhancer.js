c203d4c7304c88e64bea7308da5ae88b33edd85d
rinstar <codeninja102@gmail.com>
2018-08-30 19:51:21 -0700

    Added enhancer for project listings

---
+++ helpers/projectsListEnhancer.js
@@ -0,0 +1,25 @@
+export default ({ adapter = {} } = {}) => async ([
+  { studyUID, studyName = "", ...study } = {},
+  { status, notes, sample = false, ...project } = {}
+]) => {
+  const {
+    file: { list: fileList = () => {} } = {},
+    projects: { getProject = () => {} } = {}
+  } = adapter;
+
+  // TODO Do we need the MultusID at all?
+  const { multusID = "" } = (await getProject({ studyUID })) || {};
+
+  return {
+    ...project,
+    ...study,
+    studyName:
+      studyName.length > 20 ? studyName.substr(0, 20).concat("...") : studyName, // TODO Trim here. Maybe better place or way?
+    studyUID,
+    multusID,
+    status: status,
+    notes: notes,
+    uploadedFiles: await fileList({ path: studyUID }),
+    sample
+  };
+};

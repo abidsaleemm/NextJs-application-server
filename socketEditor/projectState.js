9fe4c0b420d528f4aac86d733d932b48ad8c4a2c
rinstar <codeninja102@gmail.com>
2018-09-04 17:54:43 -0700

    Initial payload optimization

--- socketEditor/projectState.js
+++ socketEditor/projectState.js
@@ -1,14 +1,34 @@
 import { adapter } from "../server";
+import { values, mergeWith, merge, indexBy, prop } from "ramda";
+
+const arrayMergeKey = ({ key, left, right }) =>
+  values(mergeWith(merge, indexBy(prop(key), left), indexBy(prop(key), right)));
+
+// TODO Create a module proxy for this and move code for other modules spine, shoulder, etc.
+const stateProxy = ({ vertebra, ...state }) => ({
+  vertebra: vertebraPayload
+}) => {
+  return {
+    ...state,
+    vertebra: vertebraPayload
+      ? arrayMergeKey({ key: "name", left: vertebra, right: vertebraPayload })
+      : vertebra
+  };
+};
 
 export default async ({ socket, action }) => {
   const {
-    projects: { setProjectSnapshot = () => {} } = {}
+    projects: {
+      setProjectSnapshot = () => {},
+      getProjectSnapshot = () => {}
+    } = {}
   } = adapter;
 
   const { payload = {} } = action;
   const { studyUID } = payload;
 
-  console.log("Saving snapshot", studyUID);
+  const existingSnapshot = await getProjectSnapshot({ studyUID });
+  const payloadUpdated = stateProxy(existingSnapshot)(payload);
 
-  setProjectSnapshot({ studyUID, payload });
+  setProjectSnapshot({ studyUID, payload: payloadUpdated });
 };

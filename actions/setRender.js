cae6dd33c6de14e860d14a3da3353027103b1533
rinstar <codeninja102@gmail.com>
2018-09-06 00:04:22 -0700

    Fixed setRender action was not passing template prop

--- actions/setRender.js
+++ actions/setRender.js
@@ -1,6 +1,7 @@
-export default ({ studyUID, anonymous, debug }) => ({
+export default ({ studyUID, template, anonymous, debug }) => ({
   type: "server/setRender",
   studyUID,
+  template,
   anonymous,
   debug
 });
--- socketServer/setRender.js
+++ socketServer/setRender.js
@@ -1,12 +1,13 @@
 import { adapter } from "../server";
 
 export default async ({
-  action: { studyUID, anonymous = false, debug = false } = {}
+  action: { studyUID, template, anonymous = false, debug = false } = {}
 }) => {
   const { renders: { setRenderQueue = () => {} } = {} } = adapter;
 
   await setRenderQueue({
     studyUID,
+    template,
     debug,
     anonymous
   });

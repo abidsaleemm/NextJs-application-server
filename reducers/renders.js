56c812ca2a47f7f009c326d50483738e3c75d072
rinstar <codeninja102@gmail.com>
2018-09-10 14:55:07 -0700

    renders reucer cleanup

--- reducers/renders.js
+++ reducers/renders.js
@@ -1,31 +1,51 @@
-import { SET_RENDER, DEL_RENDER } from "../constants/actionTypes";
+import {
+  SET_RENDER,
+  DEL_RENDER,
+  PAYLOAD_RENDERS
+} from "../constants/actionTypes";
 
 export const initialState = [];
 
 export default (
   state = initialState,
-  { type, studyUID, template, anonymous = false, debug = false }
+  {
+    type,
+    studyUID,
+    template,
+    anonymous = false,
+    debug = false,
+    payload = [],
+    ...props
+  }
 ) => {
   switch (type) {
     case DEL_RENDER:
       return state.filter(
-          render =>
-            render.studyUID != studyUID ||
-            render.template != template ||
-            render.anonymous != anonymous ||
-            render.debug != debug
-        );
-    case SET_RENDER:
-      if (
-        !state.filter(
-          render =>
-            render.studyUID == studyUID &&
-            render.template == template &&
-            render.anonymous == anonymous &&
-            render.debug == debug
-        ).length
-      )
-        return state.concat({ studyUID, template, anonymous, debug });
+        render =>
+          render.studyUID != studyUID ||
+          render.template != template ||
+          render.anonymous != anonymous ||
+          render.debug != debug
+      );
+    case SET_RENDER: {
+      const index = state.findIndex(
+        render =>
+          render.studyUID == studyUID &&
+          render.template == template &&
+          render.anonymous == anonymous &&
+          render.debug == debug
+      );
+
+      return index >= 0
+        ? [
+            ...state.slice(0, index),
+            { ...state[index], ...props },
+            ...state.slice(index + 1)
+          ]
+        : [...state, { ...props, studyUID, template, anonymous, debug }];
+    }
+    case PAYLOAD_RENDERS:
+      return [...payload];
     default:
       return state;
   }

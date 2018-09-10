d9cbbc09957abcb46f77c0cbb74d6c03fd6219c7
rinstar <codeninja102@gmail.com>
2018-09-10 15:57:55 -0700

    Added render broadcast actions

--- socketServer/delRender.js
+++ socketServer/delRender.js
@@ -1,11 +1,23 @@
+import { DEL_RENDER } from "../constants/actionTypes";
+
 import { adapter } from "../server";
 
 export default async ({
+  socket,
   action: { studyUID, template, anonymous, debug } = {}
 }) => {
   const { renders: { delRenderQueue = () => {} } = {} } = adapter;
 
   await delRenderQueue({
+    studyUID,
+    template,
+    debug,
+    anonymous
+  });
+
+  // Pass same action to other connected clients
+  socket.broadcast.emit("action", {
+    type: DEL_RENDER,
     studyUID,
     template,
     debug,
--- socketServer/setRender.js
+++ socketServer/setRender.js
@@ -1,11 +1,25 @@
+import { SET_RENDER } from "../constants/actionTypes";
+
 import { adapter } from "../server";
 
 export default async ({
-  action: { studyUID, template, anonymous, debug } = {}
+  socket,
+  action: { type, studyUID, template, anonymous, debug, ...props } = {}
 }) => {
   const { renders: { setRenderQueue = () => {} } = {} } = adapter;
 
   await setRenderQueue({
+    ...props,
+    studyUID,
+    template,
+    debug,
+    anonymous
+  });
+
+  // Pass same action to other connected clients
+  socket.broadcast.emit("action", {
+    type: SET_RENDER,
+    ...props,
     studyUID,
     template,
     debug,

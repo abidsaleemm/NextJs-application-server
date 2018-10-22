9444801a4e679b72750ef510e93c17264fc0638b
rinstar <codeninja102@gmail.com>
2018-10-22 12:54:01 -0700

    Added color codes

--- components/RenderFilter.js
+++ components/RenderFilter.js
@@ -6,21 +6,35 @@ import {
   Table
 } from "reactstrap";
 
+// TODO This is duplicated in application-interface.
+// Handle as import when refactor of iframe to component gets completed. WG
+const filterDefaultRender = ({ seriesName }) => {
+  const regExRemove = [/3-pl/gi, /O-Ax/, /\bloc\b/i, /scout/gi];
+  const regExAllow = [/T2/gi, /GE/gi];
+
+  return (
+    !regExRemove.some(v => v.test(seriesName)) &&
+    regExAllow.some(v => v.test(seriesName))
+  );
+};
+
 export default ({
   series = [],
   onChangeWhiteList = () => {},
   onChangeBlackList = () => {}
 }) => {
-    const regExRemove = [/3-pl/gi, /O-Ax/, /\bloc\b/i, /scout/gi];
-  const regExAllow = [/T2/gi, /GE/gi];
+  // filterDefaultRender
+  // TODO This is duplicated in application-interface.
+  // Handle as import when refactor of iframe to component gets completed. WG
+  //   const regExRemove = [/3-pl/gi, /O-Ax/, /\bloc\b/i, /scout/gi];
+  //   const regExAllow = [/T2/gi, /GE/gi];
 
-//   const ret = filterSeriesBlacklist(series)
-//     ? false
-//     : filterSeriesWhitelist(series)
-//       ? true
-//       : !regExRemove.some(v => v.test(seriesName)) &&
-//         regExAllow.some(v => v.test(seriesName));
-
+  //   const ret = filterSeriesBlacklist(series)
+  //     ? false
+  //     : filterSeriesWhitelist(series)
+  //       ? true
+  //       : !regExRemove.some(v => v.test(seriesName)) &&
+  //         regExAllow.some(v => v.test(seriesName));
 
   return (
     <UncontrolledDropdown>
@@ -61,8 +75,21 @@ export default ({
                 i
               ) => {
                 //
+                const shouldRender = filterDefaultRender({ seriesName });
+
                 return (
-                  <tr key={`render-filter-${seriesUID}-${i}`}>
+                  <tr
+                    key={`render-filter-${seriesUID}-${i}`}
+                    style={{
+                      background: whitelisted
+                        ? "green"
+                        : blacklisted
+                          ? "red"
+                          : shouldRender
+                            ? "#abebc6"
+                            : "#fadbd8"
+                    }}
+                  >
                     <td>
                       {seriesName.length > 20
                         ? `${seriesName.substring(0, 20)}...`

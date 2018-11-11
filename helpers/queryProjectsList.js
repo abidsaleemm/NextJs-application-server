3400511a9117db71833fbd47432329707dc2c6fa
rinstar <codeninja102@gmail.com>
2018-11-10 19:13:56 -0700

    Added list filtering higher order

--- helpers/queryProjectsList.js
+++ helpers/queryProjectsList.js
@@ -1,19 +1,67 @@
+import { where } from "ramda";
 import { adapter } from "../server";
 
 import projectsListEnhancer from "./projectsListEnhancer";
 import filterProjectsByUser from "./filterProjectsByUser";
 
-export default async ({ role, userID, userList = [] }) => {
+// TODO Add filter fuctions here for now. WG
+const statusCheck = ({ statusFilter = "", status }) =>
+  [
+    statusFilter === "",
+    statusFilter === "All",
+    statusFilter === "Not Delivered" &&
+      status !== "Delivered" &&
+      status !== "Archived",
+    statusFilter === "Start" && status === "Start",
+    statusFilter === "Segmentation" && status === "Segmentation",
+    statusFilter === "QC" && status === "QC",
+    statusFilter === "Review" && status === "Review",
+    statusFilter === "Done" && status === "Done",
+    statusFilter === "Rendered" && status === "Rendered",
+    statusFilter === "Delivered" && status === "Delivered",
+    statusFilter === "Archived" && status === "Archived"
+  ].some(v => v);
+
+const projectTypeCheck = ({ projectTypeFilter, projectType }) =>
+  projectTypeFilter === "All"
+    ? true
+    : projectTypeFilter === "Not Removed"
+      ? projectType !== "Removed"
+      : projectType === projectTypeFilter;
+
+export default async ({
+  role,
+  settings: {
+    filter: {
+      status: statusFilter = "",
+      projectType: projectTypeFilter = "All"
+    } = {}
+  } = {},
+  userID,
+  userList = []
+}) => {
   const {
     projects: { getProjectList = () => {} } = {},
     dicom: { getStudies = () => {} } = {}
   } = adapter;
 
-  // TODO Do query directly getProjectList instead of filtering with javascript
   const [projects = [], studies = []] = await Promise.all([
-    getProjectList(),
+    getProjectList({
+      filter: ({ status, projectType = "Live" }) => {
+        const pred = where({
+          status: v => statusCheck({ statusFilter, status: v }),
+          projectType: v =>
+            projectTypeCheck({ projectTypeFilter, projectType: v })
+        });
+
+        return pred({ status, projectType });
+      }
+    }),
+    // TODO Merges all studies for now?
     getStudies()
   ]);
+
+  console.log("projects", projects);
 
   // Merging studies and projects table
   const projectsList = await Promise.all(
@@ -22,10 +70,15 @@ export default async ({ role, userID, userList = [] }) => {
         study,
         projects.find(({ studyUID = "" }) => study.studyUID === studyUID)
       ])
+      .filter(
+        ([, { status = "", projectType = "Live" } = {}]) =>
+          statusCheck({ statusFilter, status }) &&
+          projectTypeCheck({ projectTypeFilter, projectType })
+      ) // TODO Adding an additional filter here.
       .filter(filterProjectsByUser({ role, userID, userList }))
       .filter(([study]) => study !== undefined)
       .filter(
-        ([study, { projectType } = {}]) =>
+        ([, { projectType } = {}]) =>
           projectType === "Removed" ? role === "admin" : true
       )
       .map(projectsListEnhancer({ adapter }))
